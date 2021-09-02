20e2ebf7316dad05552b33c7a299230a2a9811b3
Lisa Barros de Andrade e Sousa <lisa.barros.andrade.sousa@gmail.com>
2021-09-02 14:34:57 +0200

    refactoring

---
+++ src/get_probes.py
@@ -0,0 +1,135 @@
+############################################
+# imports
+############################################
+
+import os
+import pickle
+import pandas as pd
+
+from gtfparse import read_gtf
+import Levenshtein as Lev
+from Bio import SeqIO
+from Bio.SeqUtils import GC
+from Bio.SeqUtils import MeltingTemp as mt
+
+
+############################################
+# probe processing
+############################################
+
+def get_mapping_gene_exon(dir_output, file_gene_annotation):
+
+    file_mapping_exon_to_gene = '{}mapping_exon_to_gene.pkl'.format(dir_output)
+    #file_mapping_gene_to_exon = '{}mapping_gene_to_exon.pkl'.format(dir_output)
+
+    if not os.path.isfile(file_mapping_exon_to_gene): #or not os.path.isfile(file_mapping_gene_to_exon):
+
+        gene_annotation = read_gtf(file_gene_annotation)
+        exon_annotation = gene_annotation[gene_annotation["feature"] == "exon"]
+
+        if not 'exon_id' in exon_annotation.columns:
+            exon_annotation['exon_id'] = exon_annotation['transcript_id'] + '_exon' + exon_annotation['exon_number']
+            exon_annotation['source'] = 'Refseq'
+
+        mapping_exon_to_gene = pd.Series(exon_annotation.gene_id.values,index=exon_annotation.exon_id).to_dict()
+        #mapping_gene_to_exon = pd.Series(exon_annotation.exon_id.values,index=exon_annotation.gene_id).to_dict()
+
+        pickle.dump(mapping_exon_to_gene, open(file_mapping_exon_to_gene,'wb'))
+        #pickle.dump(mapping_gene_to_exon, open(file_mapping_gene_to_exon,'wb'))
+
+    return file_mapping_exon_to_gene#, file_mapping_gene_to_exon
+
+
+############################################
+
+def get_mapping_gene_probes(dir_output, file_exon_sequence, mapping_exon_to_gene, probe_length, GC_content_min, GC_content_max, Tm_min, Tm_max, logging = None):
+
+    file_mapping_gene_to_probes = '{}mapping_gene_to_probes.pkl'.format(dir_output)
+
+    if not os.path.isfile(file_mapping_gene_to_probes):
+        count_exons = 0
+        count_probes = 0
+
+        mapping_gene_to_probes = {}
+
+        for exon in SeqIO.parse(file_exon_sequence, "fasta"):
+
+            count_exons += 1
+            exon_id = exon.id.split('::')[0]
+            gene_id = mapping_exon_to_gene[exon_id]
+
+            exon_sequence = exon.seq
+            if len(exon_sequence) > probe_length:
+                probes_of_exon = set([exon_sequence[i:i+probe_length] for i in range(len(exon_sequence)-(probe_length-1)) if ('N' not in exon_sequence[i:i+probe_length])])
+                count_probes += len(probes_of_exon)
+                
+                for probe in probes_of_exon:
+                    gc_content = GC(probe)
+                    if (GC_content_min < gc_content < GC_content_max):
+                        Tm = mt.Tm_NN(probe)
+                        if (Tm_min < Tm < Tm_max):
+                            if gene_id in mapping_gene_to_probes:
+                                if probe in mapping_gene_to_probes[gene_id]:
+                                    mapping_gene_to_probes[gene_id][probe].append(exon_id)
+                                else:
+                                    mapping_gene_to_probes[gene_id][probe] = [exon_id]
+                            else:
+                                mapping_gene_to_probes[gene_id] = {probe: [exon_id]}
+                        
+            #if count_exons % 100000 == 0:
+            #    print('Processed exons: {}'.format(count_exons))
+            #    print('Probes found in total: {}'.format(count_probes))
+            #    break
+
+        if logging:
+            logging.info('Total number of exons processed: {} with {} probes in total.'.format(count_exons, count_probes))
+        pickle.dump(mapping_gene_to_probes, open(file_mapping_gene_to_probes,'wb'))
+
+    return file_mapping_gene_to_probes
+
+
+############################################
+
+def get_unique_probes(dir_output, mapping_gene_to_probes, mapping_gene_to_probes_unique, logging=None):
+
+    file_mapping_gene_to_probes_unique = '{}mapping_gene_to_probes_unique.pkl'.format(dir_output)
+
+    if not os.path.isfile(file_mapping_gene_to_probes_unique):
+
+        count_genes = 0
+        count_probes = 0
+
+        probes_unique = {}
+        probes_non_unique = {}
+
+        for gene_id, mapping_probe_to_exon in mapping_gene_to_probes.items():
+            count_genes +=1
+
+            for probe, exon_id in mapping_probe_to_exon.items():
+                count_probes +=1
+
+                if probe in probes_non_unique:
+                    probes_non_unique[probe] += 1
+                    mapping_gene_to_probes_unique[gene_id].pop(probe)
+
+                elif probe in probes_unique:
+                    mapping_gene_to_probes_unique[gene_id].pop(probe)
+                    mapping_gene_to_probes_unique[probes_unique[probe][1]].pop(probe)
+                    probes_unique.pop(probe)
+                    probes_non_unique[probe] = 2
+
+                else:
+                    probes_unique[probe] = [exon_id, gene_id]
+
+                #if count_probes % 100000 == 0:
+                #    print('Processed genes: {}'.format(count_genes))
+                #    print('Processed probes: {}'.format(count_probes))
+                #    print('Unique probes: {}'.format(len(probes_unique)))
+        
+        if logging:
+            logging.info('Total number of genes processed: {}, total number of probes processed: {} with {} unique probes in total.'.format(count_genes, count_probes, len(probes_unique)))    
+        pickle.dump(mapping_gene_to_probes_unique, open(file_mapping_gene_to_probes_unique,'wb')) 
+    
+    return file_mapping_gene_to_probes_unique
+
+    ############################################
--- src/load_exome.py
+++ src/load_exome.py
@@ -4,38 +4,18 @@
 ############################################
 
 import os
-import argparse
 import itertools
 import pybedtools
 from gtfparse import read_gtf
 from Bio import SeqIO
 from ftplib import FTP
 import pandas as pd
+import subprocess as sp
+
 
 ############################################
 # pipeline
 ############################################
-
-def args():
-    """
-    Returns command line arguments. 
-    Parameters
-    ----------
-        --- none ---
-
-    Returns
-    -------
-        args_parser: namespace
-            Namespace object with argument attributes.
-    """
-    
-    args_parser = argparse.ArgumentParser(description=__doc__,formatter_class=argparse.RawDescriptionHelpFormatter)
-    args_parser.add_argument('-d','--dirdata',help='directory for data storage',type=str,required=True)
-    args_parser.add_argument('-s','--source',help='download data from "ensemble" or "ncbi"',type=str,required=True)
-    args_parser.add_argument('-r','--release_ensemble',help='ensemble release',type=str,required=False, default='104')
-    return args_parser.parse_args()
-
-
 
 def ftp_download(dir_data, ftp_link, directory, file_pattern):
     # login and move to directory
@@ -62,6 +42,7 @@ def ftp_download(dir_data, ftp_link, directory, file_pattern):
     return file_output
 
 
+############################################
 
 def download_gene_gtf(dir_data, source, release_ensemble):
     # ensemble: release_ensemble = 104
@@ -79,12 +60,14 @@ def download_gene_gtf(dir_data, source, release_ensemble):
     file_output = ftp_download(dir_data, ftp_link, directory, file_pattern)
 
     file_gene_annotation = '{}{}'.format(dir_data, file_output)
-    os.system("gunzip {}".format(file_gene_annotation))
+    cmd = "gunzip {}".format(file_gene_annotation)
+    sp.run(cmd, shell=True)
     file_gene_annotation = file_gene_annotation.split('.gz')[0]
         
     return file_gene_annotation
 
 
+############################################
 
 def download_genome_fasta(dir_data, source, release_ensemble, file_chr_mapping):
     # ensemble: release = 104
@@ -102,7 +85,8 @@ def download_genome_fasta(dir_data, source, release_ensemble, file_chr_mapping):
     file_output = ftp_download(dir_data, ftp_link, directory, file_pattern)
 
     file_genome_sequence = '{}{}'.format(dir_data, file_output)
-    os.system("gunzip {}".format(file_genome_sequence))
+    cmd = "gunzip {}".format(file_genome_sequence)
+    sp.run(cmd, shell=True)
     file_genome_sequence = file_genome_sequence.split('.gz')[0]
 
     if source == 'ncbi':
@@ -111,6 +95,7 @@ def download_genome_fasta(dir_data, source, release_ensemble, file_chr_mapping):
     return file_genome_sequence
 
 
+############################################
 
 def download_chr_mapping(dir_data):
     ftp_link = 'ftp.ncbi.nlm.nih.gov'
@@ -121,10 +106,10 @@ def download_chr_mapping(dir_data):
 
     file_mapping = '{}{}'.format(dir_data, file_output)
 
-    with open(file_mapping) as handle:
-        *_comments, names = itertools.takewhile(lambda line: line.startswith('#'), handle)
+    with open(file_mapping) as output:
+        *_comments, names = itertools.takewhile(lambda line: line.startswith('#'), output)
         names = names[1:].split()
-    handle.close()
+    output.close()
 
     assembly_report = pd.read_table(file_mapping, names=names, sep="\t", comment='#')
 
@@ -140,6 +125,7 @@ def download_chr_mapping(dir_data):
     return mapping
 
 
+############################################
 
 def process_ncbi_genome(dir_data, file_genome_sequence, mapping):
     
@@ -157,9 +143,28 @@ def process_ncbi_genome(dir_data, file_genome_sequence, mapping):
         else:
             print('No mapping for accession number: {}'.format(accession_number))
     
-    os.system('mv {} {}'.format(file_output, file_genome_sequence))
+    cmd = 'mv {} {}'.format(file_output, file_genome_sequence)
+    sp.run(cmd, shell=True)
 
 
+############################################
+
+def process_ncbi_annotation(exon_annotation, mapping):
+
+    exon_annotation['exon_id'] = exon_annotation['transcript_id'] + '_exon' + exon_annotation['exon_number']
+    exon_annotation['source'] = 'Refseq'
+
+    accession_numbers = exon_annotation.seqname.unique()
+    for accession_number in accession_numbers:
+        if accession_number in mapping:
+            exon_annotation.seqname[exon_annotation.seqname == accession_number] = mapping[accession_number]
+        else:
+            print('No mapping for accession number: {}'.format(accession_number))
+            exon_annotation = exon_annotation[exon_annotation.seqname != accession_number]
+    return exon_annotation
+
+
+############################################
 
 def get_exons_annotation(file_gene_annotation, source, file_chr_mapping):
 
@@ -176,22 +181,7 @@ def get_exons_annotation(file_gene_annotation, source, file_chr_mapping):
     return file_exon_annotation
 
 
-
-def process_ncbi_annotation(exon_annotation, mapping):
-
-    exon_annotation['exon_id'] = exon_annotation['transcript_id'] + '_exon' + exon_annotation['exon_number']
-    exon_annotation['source'] = 'Refseq'
-
-    accession_numbers = exon_annotation.seqname.unique()
-    for accession_number in accession_numbers:
-        if accession_number in mapping:
-            exon_annotation.seqname[exon_annotation.seqname == accession_number] = mapping[accession_number]
-        else:
-            print('No mapping for accession number: {}'.format(accession_number))
-            exon_annotation = exon_annotation[exon_annotation.seqname != accession_number]
-    return exon_annotation
-
-    
+############################################
 
 def get_exons_fasta(file_exon_annotation, file_genome_sequence):
     
@@ -204,44 +194,11 @@ def get_exons_fasta(file_exon_annotation, file_genome_sequence):
     exon_annotation = exon_annotation.sequence(fi=genome_sequence, s=True, name=True)
     exon_annotation.save_seqs(file_exon_sequence)
     
+    cmd = 'rm {}.fai'.format(file_genome_sequence)
+    sp.run(cmd, shell=True)
+
     return file_exon_sequence
 
 
+############################################
 
-def main():
-    
-    parameters = args()
-
-    dir_data = parameters.dirdata
-    source = parameters.source
-    release_ensemble = parameters.release_ensemble
-
-    print('\nDownload data from: {}'.format(source))
-
-    if source == 'ncbi':
-        # if source ncbi we need a chromosome mapping
-        mapping = download_chr_mapping(dir_data)
-    else:
-        mapping = None
-    if source == 'ensemble':
-        print('Using Ensemble release {}'.format(release_ensemble))
-
-    file_gene_annotation = download_gene_gtf(dir_data, source, release_ensemble)
-    print('\nFile gene annotation: {}'.format(file_gene_annotation))
-
-    print('\nget exome annotation')
-    file_exon_annotation = get_exons_annotation(file_gene_annotation, source, mapping)
-    print('File exon annotation: {}'.format(file_exon_annotation))
-
-    file_genome_sequence = download_genome_fasta(dir_data, source, release_ensemble, mapping)
-    print('File genome sequence: {}'.format(file_genome_sequence))
-
-    print('\nget exome fasta')
-    file_exon_sequence = get_exons_fasta(file_exon_annotation, file_genome_sequence)
-    print('File exon sequence: {}'.format(file_exon_sequence))
-
-
-
-if __name__ == '__main__':
-    
-    main()
