5b294c8b266772eba1db32cdf932e015788f521a
Lisa Barros de Andrade e Sousa <lisa.barros.andrade.sousa@gmail.com>
2021-09-08 16:35:03 +0200

    changed blast filter to conda blast

--- src/filter_probes.py
+++ src/filter_probes.py
@@ -13,207 +13,114 @@ import Levenshtein as Lev
 
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
+from Bio.Blast.Applications import NcbiblastnCommandline
+from Bio.Blast.Applications import NcbimakeblastdbCommandline
 
 ############################################
 # filter probes with blastn or hamming distance 
 ############################################
 
 
-def filter_probes_with_blastn(dir_output, file_exon_sequence, mapping_gene_to_probes_unique, mapping_exon_to_gene, min_probes_per_gene, probe_length, coverage, percent_identity, num_threads):
+def filter_probes_with_blastn(dir_output, file_exon_sequence, mapping_gene_to_probes_unique, mapping_exon_to_gene, min_probes_per_gene, probe_length, word_size, coverage, percent_identity, num_threads):
   
-    print('Filter 1')
-    file_mapping_gene_to_probes_blastn = '{}mapping_gene_to_probes_blastn.pkl'.format(dir_output)
+    #print_run = 500
+    count_genes = 0
+    count_probes = 0
+    count_probes_last_run = 0
+    count_selected_probes = 0
 
-    if not os.path.isfile(file_mapping_gene_to_probes_blastn):
-        print_run = 50
-        count_genes = 0
-        count_probes = 0
-        count_probes_last_run = 0
-        count_selected_probes = 0
+    # create temporary directory
+    dir_output_blast = '{}probe_queries_combined/'.format(dir_output)
+    cmd = 'mkdir {}'.format(dir_output_blast)
+    sp.run(cmd, shell=True)
+
+    # create blast database
+    cmd = NcbimakeblastdbCommandline(input_file=file_exon_sequence, dbtype='nucl')
+    out, err = cmd()
+
+    t = time.time()
+    mapping_gene_to_probes_blastn = {}
+
+    probes = []
+    for gene_id, mapping_probe_to_exon in mapping_gene_to_probes_unique.items():
+        count_genes += 1
+        #if count_genes > print_run:
+        #    break
         
-        dir_output_blast = '{}probe_queries_single/'.format(dir_output)
-        cmd = 'mkdir {}'.format(dir_output_blast)
-        sp.run(cmd, shell=True)
-
-        cmd = 'makeblastdb -in {} -dbtype nucl'.format(file_exon_sequence)
-        sp.run(cmd, shell=True)
-
-        t = time.time()
-        mapping_gene_to_probes_blastn = {}
-
-        for gene_id, mapping_probe_to_exon in mapping_gene_to_probes_unique.items():
-            if count_genes > 1000:
-                return
-            count_genes += 1
+        # exclude genes with less than required number of probes
+        if len(mapping_probe_to_exon) > min_probes_per_gene:
             
-            # exclude genes with less than required number of probes
-            if len(mapping_probe_to_exon) > min_probes_per_gene:
+            # write all probes to fasta file
+            for probe, exons in mapping_probe_to_exon.items():
+                count_probes += 1
+                sequence = SeqRecord(probe, '{}_{}'.format(gene_id, probe), '', '')
+                probes.append(sequence)
+
+        else:
+            mapping_gene_to_probes_blastn[gene_id] = None
+
+    file_fasta_probes_per_gene = '{}probes.fna'.format(dir_output_blast)
                 
-                # write all probes to fasta file
-                probes = []
-               
-                for probe, exons in mapping_probe_to_exon.items():
-                    count_probes += 1
-                    sequence = SeqRecord(probe, '{}'.format(probe), '', '')
-                    probes.append(sequence)
-
-                file_fasta_probes_per_gene = '{}probes_{}.fna'.format(dir_output_blast, gene_id)
-                
-                output = open(file_fasta_probes_per_gene, 'w')
-                SeqIO.write(probes, output, 'fasta')
-                output.close()
-
-                # run BlastN
-                file_blast_output = '{}blast_{}.txt'.format(dir_output_blast, gene_id)
-                cmd = 'blastn -query {} -db {} -outfmt 10 -out {} -word_size 7 -strand plus -num_threads {} 2>/dev/null'.format(file_fasta_probes_per_gene, file_exon_sequence, file_blast_output, num_threads)
-                sp.run(cmd, shell=True)
-
-                # read and process results of BlastN
-                blast_results = pd.read_csv(file_blast_output,header=None, usecols=[0,1,2,3,6,7]) # don't load columns: 'missmatches','gap_opens','start_t','end_t', 'evalue','bit_score'
-                blast_results.columns = ['probe_query','exon_target','percent_identity','alignment_length','start_q','end_q']
-                blast_results['exon_target'] = blast_results['exon_target'].str.split('::').str[0]
-                
-                probes_with_match = []
-                for idx in blast_results.index:
-                    probe = blast_results['probe_query'][idx]
-                    gene_id_target = mapping_exon_to_gene[blast_results['exon_target'][idx]]
-
-                    # only consider hit in other genes
-                    if gene_id_target != gene_id:
-                        # filter out probes that have more than 50% sequence coverage with target sequence, 80% identity with target sequence, and target sequence covers ligation site +- 5
-                        if int(blast_results['alignment_length'][idx]) > (probe_length * coverage) and float(blast_results['percent_identity'][idx]) > percent_identity and int(blast_results['start_q'][idx]) < (probe_length //2 - 4) and int(blast_results['end_q'][idx]) > (probe_length //2 + 5):
-                            probes_with_match.append(probe)
-
-                # remove all probes with matches
-                for probe in set(probes_with_match):
-                    mapping_probe_to_exon.pop(probe)
-                count_selected_probes += len(mapping_probe_to_exon)
-                mapping_gene_to_probes_blastn[gene_id] = mapping_probe_to_exon
-
-            
-            if count_genes % print_run == 0:
-                count_probes_single_run = count_probes - count_probes_last_run
-                time_elapsed = time.time() - t
-
-                print('In total, {} genes with {} probes were processed and {} probes passed the blastn filter in total.'.format(count_genes, count_probes, count_selected_probes))
-                print('In one run, {} genes with {} probes were processed, which took {} s and {} s per probe'.format(print_run,count_probes_single_run,time_elapsed,time_elapsed/count_probes_single_run))
-               
-                count_probes_last_run = count_probes
-                t = time.time()
-
-        print('Total number of genes processed: {}, total number of probes processed: {} with {} probes passed hamming distance filter.'.format(count_genes, count_probes, count_selected_probes))    
-        pickle.dump(mapping_gene_to_probes_blastn, open(file_mapping_gene_to_probes_blastn,'wb')) 
-
-        # remove blast files
-        cmd = 'rm -rf {}'.format(dir_output_blast)
-        sp.run(cmd, shell=True)
-
-    return file_mapping_gene_to_probes_blastn
+    output = open(file_fasta_probes_per_gene, 'w')
+    SeqIO.write(probes, output, 'fasta')
+    output.close()
 
 
+    # run BlastN
+    file_blast_output = '{}blast.txt'.format(dir_output_blast)
+    cmd = NcbiblastnCommandline(query=file_fasta_probes_per_gene,db=file_exon_sequence, outfmt=10, out=file_blast_output,word_size=word_size, strand='plus',num_threads=num_threads)
+    out, err = cmd()
 
-############################################
-
-def filter_probes_with_blastn_2(dir_output, file_exon_sequence, mapping_gene_to_probes_unique, mapping_exon_to_gene, min_probes_per_gene, probe_length, coverage, percent_identity, num_threads):
-  
-    print('Filter 2')
-    file_mapping_gene_to_probes_blastn = '{}mapping_gene_to_probes_blastn.pkl'.format(dir_output)
-
-    if not os.path.isfile(file_mapping_gene_to_probes_blastn):
-        print_run = 100
-        count_genes = 0
-        count_probes = 0
-        count_probes_last_run = 0
-        count_selected_probes = 0
-
-        dir_output_blast = '{}probe_queries_combined/'.format(dir_output)
-        cmd = 'mkdir {}'.format(dir_output_blast)
-        sp.run(cmd, shell=True)
-
-        cmd = 'makeblastdb -in {} -dbtype nucl'.format(file_exon_sequence)
-        sp.run(cmd, shell=True)
-
-        t = time.time()
-        mapping_gene_to_probes_blastn = {}
-
-        probes = []
-        for gene_id, mapping_probe_to_exon in mapping_gene_to_probes_unique.items():
-            count_genes += 1
-            if count_genes > print_run:
-                break
-            
-            # exclude genes with less than required number of probes
-            if len(mapping_probe_to_exon) > min_probes_per_gene:
-                
-                # write all probes to fasta file
-                for probe, exons in mapping_probe_to_exon.items():
-                    count_probes += 1
-                    sequence = SeqRecord(probe, '{}_{}'.format(gene_id, probe), '', '')
-                    probes.append(sequence)
-
-        file_fasta_probes_per_gene = '{}probes.fna'.format(dir_output_blast)
-                    
-        output = open(file_fasta_probes_per_gene, 'w')
-        SeqIO.write(probes, output, 'fasta')
-        output.close()
-
-        # run BlastN
-        file_blast_output = '{}blast.txt'.format(dir_output_blast)
-        cmd = 'blastn -query {} -db {} -outfmt 10 -out {} -word_size 7 -strand plus -num_threads {} 2>{}blast_log.txt'.format(file_fasta_probes_per_gene, file_exon_sequence, file_blast_output, num_threads, dir_output_blast)
-        sp.run(cmd, shell=True)
-
-        # read and process results of BlastN
-        blast_results = pd.read_csv(file_blast_output,header=None, usecols=[0,1,2,3,6,7]) # don't load columns: 'missmatches','gap_opens','start_t','end_t', 'evalue','bit_score'
-        blast_results.columns = ['query','target_exon','percent_identity','alignment_length','query_start','query_end']
-        
-        blast_results['query_gene_id'] = blast_results['query'].str.split('_').str[0]
-        blast_results['query_probe'] = blast_results['query'].str.split('_').str[1]
-        blast_results['target_exon'] = blast_results['target_exon'].str.split('::').str[0]
-        
-        
-        for gene_id in blast_results['query_gene_id'].unique():
-            probes_with_match = []
-            blast_results_gene = blast_results[blast_results['query_gene_id'] == gene_id]
-            blast_results_gene.reset_index(inplace=True, drop=True)
-            for idx in blast_results_gene.index:
-                probe = blast_results_gene['query_probe'][idx]
-                gene_id_target = mapping_exon_to_gene[blast_results_gene['target_exon'][idx]]
-
-                # only consider hit in other genes
-                if gene_id_target != gene_id:
-                    # filter out probes that have more than 50% sequence coverage with target sequence, 80% identity with target sequence, and target sequence covers ligation site +- 5
-                    if int(blast_results_gene['alignment_length'][idx]) > (probe_length * coverage) and float(blast_results_gene['percent_identity'][idx]) > percent_identity and int(blast_results_gene['query_start'][idx]) < (probe_length //2 - 4) and int(blast_results_gene['query_end'][idx]) > (probe_length //2 + 5):
-                        probes_with_match.append(probe)
-
-            # remove all probes with matches
-            mapping_probe_to_exon = mapping_gene_to_probes_unique[gene_id]
-            for probe in set(probes_with_match):
-                mapping_probe_to_exon.pop(probe)
-            mapping_gene_to_probes_blastn[gene_id] = mapping_probe_to_exon
-            count_selected_probes += len(mapping_probe_to_exon)
-        
-                
-        count_probes_single_run = count_probes - count_probes_last_run
-        time_elapsed = time.time() - t
-
-        print('In total, {} genes with {} probes were processed and {} probes passed the blastn filter in total.'.format(count_genes, count_probes, count_selected_probes))
-        print('In one run, {} genes with {} probes were processed, which took {} s and {} s per probe'.format(print_run,count_probes_single_run,time_elapsed,time_elapsed/count_probes_single_run))
+    # read and process results of BlastN
+    blast_results = pd.read_csv(file_blast_output,header=None, usecols=[0,1,2,3,6,7]) # don't load columns: 'missmatches','gap_opens','start_t','end_t', 'evalue','bit_score'
+    blast_results.columns = ['query','target_exon','percent_identity','alignment_length','query_start','query_end']
     
-        count_probes_last_run = count_probes
-        t = time.time()
-                    
-        #print('Total number of genes processed: {}, total number of probes processed: {} with {} probes passed hamming distance filter.'.format(count_genes, count_probes, count_selected_probes))    
-        #pickle.dump(mapping_gene_to_probes_blastn, open(file_mapping_gene_to_probes_blastn,'wb')) 
+    blast_results['query_gene_id'] = blast_results['query'].str.split('_').str[0]
+    blast_results['query_probe'] = blast_results['query'].str.split('_').str[1]
+    blast_results['target_exon'] = blast_results['target_exon'].str.split('::').str[0]
+    
+    
+    for gene_id in blast_results['query_gene_id'].unique():
+        probes_with_match = []
+        blast_results_gene = blast_results[blast_results['query_gene_id'] == gene_id]
+        blast_results_gene.reset_index(inplace=True, drop=True)
+        for idx in blast_results_gene.index:
+            probe = blast_results_gene['query_probe'][idx]
+            gene_id_target = mapping_exon_to_gene[blast_results_gene['target_exon'][idx]]
 
-        # remove blast files
-        cmd = 'rm -rf {}'.format(dir_output_blast)
-        #sp.run(cmd, shell=True)
+            # only consider hit in other genes
+            if gene_id_target != gene_id:
+                # filter out probes that have more than 50% sequence coverage with target sequence, 80% identity with target sequence, and target sequence covers ligation site +- 5
+                if int(blast_results_gene['alignment_length'][idx]) > (probe_length * coverage) and float(blast_results_gene['percent_identity'][idx]) > percent_identity and int(blast_results_gene['query_start'][idx]) < (probe_length //2 - 4) and int(blast_results_gene['query_end'][idx]) > (probe_length //2 + 5):
+                    probes_with_match.append(probe)
 
-    return file_mapping_gene_to_probes_blastn
+        # remove all probes with matches
+        mapping_probe_to_exon = mapping_gene_to_probes_unique[gene_id]
+        for probe in set(probes_with_match):
+            mapping_probe_to_exon.pop(probe)
+        mapping_gene_to_probes_blastn[gene_id] = mapping_probe_to_exon
+        count_selected_probes += len(mapping_probe_to_exon)
+    
+            
+    count_probes_single_run = count_probes - count_probes_last_run
+    time_elapsed = time.time() - t
+
+    print('In total, {} genes with {} probes were processed and {} probes passed the blastn filter in total.'.format(count_genes, count_probes, count_selected_probes))
+    print('In one run, {} genes with {} probes were processed, which took {} s and {} s per probe'.format(count_genes,count_probes_single_run,time_elapsed,time_elapsed/count_probes_single_run))
+
+    count_probes_last_run = count_probes
+    t = time.time()
+
+    # remove blast files
+    cmd = 'rm -rf {}'.format(dir_output_blast)
+    sp.run(cmd, shell=True)
+
+    return mapping_gene_to_probes_blastn
 
 
 ############################################
 
+'''
 def filter_probes_with_hamming_distance(dir_output, mapping_gene_to_probes_unique, min_probes_per_gene, probe_length, percent_identity):
   
     file_mapping_gene_to_probes_hamming = '{}mapping_gene_to_probes_hamming.pkl'.format(dir_output)
@@ -269,6 +176,9 @@ def filter_probes_with_hamming_distance(dir_output, mapping_gene_to_probes_uniqu
         pickle.dump(mapping_gene_to_probes_hamming, open(file_mapping_gene_to_probes_hamming,'wb')) 
     
     return file_mapping_gene_to_probes_hamming
+
+
+'''
 
 
 
--- src/get_probes.py
+++ src/get_probes.py
@@ -4,10 +4,11 @@
 
 import os
 import pickle
+import logging
 import pandas as pd
 
 from gtfparse import read_gtf
-import Levenshtein as Lev
+
 from Bio import SeqIO
 from Bio.SeqUtils import GC
 from Bio.SeqUtils import MeltingTemp as mt
@@ -17,12 +18,11 @@ from Bio.SeqUtils import MeltingTemp as mt
 # probe processing
 ############################################
 
-def get_mapping_gene_exon(dir_output, file_gene_annotation):
+def get_mapping_gene_exon(dir_output, file_gene_annotation, get_annotation):
 
     file_mapping_exon_to_gene = '{}mapping_exon_to_gene.pkl'.format(dir_output)
-    #file_mapping_gene_to_exon = '{}mapping_gene_to_exon.pkl'.format(dir_output)
 
-    if not os.path.isfile(file_mapping_exon_to_gene): #or not os.path.isfile(file_mapping_gene_to_exon):
+    if get_annotation and not os.path.isfile(file_mapping_exon_to_gene): 
 
         gene_annotation = read_gtf(file_gene_annotation)
         exon_annotation = gene_annotation[gene_annotation["feature"] == "exon"]
@@ -32,21 +32,21 @@ def get_mapping_gene_exon(dir_output, file_gene_annotation):
             exon_annotation['source'] = 'Refseq'
 
         mapping_exon_to_gene = pd.Series(exon_annotation.gene_id.values,index=exon_annotation.exon_id).to_dict()
-        #mapping_gene_to_exon = pd.Series(exon_annotation.exon_id.values,index=exon_annotation.gene_id).to_dict()
-
         pickle.dump(mapping_exon_to_gene, open(file_mapping_exon_to_gene,'wb'))
-        #pickle.dump(mapping_gene_to_exon, open(file_mapping_gene_to_exon,'wb'))
+    
+    else:
+        mapping_exon_to_gene = pickle.load(open(file_mapping_exon_to_gene,'rb'))
 
-    return file_mapping_exon_to_gene#, file_mapping_gene_to_exon
+    return mapping_exon_to_gene
 
 
 ############################################
 
-def get_mapping_gene_probes(dir_output, file_exon_sequence, mapping_exon_to_gene, probe_length, GC_content_min, GC_content_max, Tm_min, Tm_max, logging = None):
+def get_mapping_gene_probes(dir_output, file_exon_sequence, mapping_exon_to_gene, probe_length, GC_content_min, GC_content_max, Tm_min, Tm_max, get_annotation):
 
     file_mapping_gene_to_probes = '{}mapping_gene_to_probes.pkl'.format(dir_output)
 
-    if not os.path.isfile(file_mapping_gene_to_probes):
+    if get_annotation and not os.path.isfile(file_mapping_gene_to_probes):
         count_exons = 0
         count_probes = 0
 
@@ -70,31 +70,34 @@ def get_mapping_gene_probes(dir_output, file_exon_sequence, mapping_exon_to_gene
                         if (Tm_min < Tm < Tm_max):
                             if gene_id in mapping_gene_to_probes:
                                 if probe in mapping_gene_to_probes[gene_id]:
-                                    mapping_gene_to_probes[gene_id][probe].append(exon_id)
+                                    mapping_gene_to_probes[gene_id][probe]['exon_id'].append(exon_id)
                                 else:
-                                    mapping_gene_to_probes[gene_id][probe] = [exon_id]
+                                    mapping_gene_to_probes[gene_id][probe] = {'exon_id': [exon_id], 'GC': gc_content, 'Tm': Tm}
                             else:
-                                mapping_gene_to_probes[gene_id] = {probe: [exon_id]}
+                                mapping_gene_to_probes[gene_id] = {probe: {'exon_id': [exon_id], 'GC': gc_content, 'Tm': Tm}}
                         
-            #if count_exons % 100000 == 0:
+            #if count_exons % 1000 == 0:
             #    print('Processed exons: {}'.format(count_exons))
             #    print('Probes found in total: {}'.format(count_probes))
             #    break
 
-        if logging:
-            logging.info('Total number of exons processed: {} with {} probes in total.'.format(count_exons, count_probes))
         pickle.dump(mapping_gene_to_probes, open(file_mapping_gene_to_probes,'wb'))
 
-    return file_mapping_gene_to_probes
+        logging.info('In total {} exons processed with {} probes.'.format(count_exons, count_probes))
+
+    else:
+        mapping_gene_to_probes = pickle.load(open(file_mapping_gene_to_probes,'rb'))
+
+    return mapping_gene_to_probes
 
 
 ############################################
 
-def get_unique_probes(dir_output, mapping_gene_to_probes, mapping_gene_to_probes_unique, logging=None):
+def get_unique_probes(dir_output, mapping_gene_to_probes, mapping_gene_to_probes_unique, get_annotation):
 
     file_mapping_gene_to_probes_unique = '{}mapping_gene_to_probes_unique.pkl'.format(dir_output)
 
-    if not os.path.isfile(file_mapping_gene_to_probes_unique):
+    if get_annotation and not os.path.isfile(file_mapping_gene_to_probes_unique):
 
         count_genes = 0
         count_probes = 0
@@ -125,11 +128,14 @@ def get_unique_probes(dir_output, mapping_gene_to_probes, mapping_gene_to_probes
                 #    print('Processed genes: {}'.format(count_genes))
                 #    print('Processed probes: {}'.format(count_probes))
                 #    print('Unique probes: {}'.format(len(probes_unique)))
-        
-        if logging:
-            logging.info('Total number of genes processed: {}, total number of probes processed: {} with {} unique probes in total.'.format(count_genes, count_probes, len(probes_unique)))    
+               
         pickle.dump(mapping_gene_to_probes_unique, open(file_mapping_gene_to_probes_unique,'wb')) 
+
+        logging.info('Total number of genes processed: {}, total number of probes processed: {} with {} unique probes in total.'.format(count_genes, count_probes, len(probes_unique)))
     
-    return file_mapping_gene_to_probes_unique
+    else:
+        mapping_gene_to_probes_unique = pickle.load(open(file_mapping_gene_to_probes_unique,'rb'))
+
+    return mapping_gene_to_probes_unique
 
     ############################################
