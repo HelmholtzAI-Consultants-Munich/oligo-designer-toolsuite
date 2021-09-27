bbaa5b21802f9470852b29733788afdf8bcd13b7
Lisa Barros de Andrade e Sousa <lisa.barros.andrade.sousa@gmail.com>
2021-09-27 20:24:41 +0200

    added Tm parameter option, fixed logging

--- probe_design.py
+++ probe_design.py
@@ -2,17 +2,20 @@
 # imports
 ############################################
 
+from datetime import datetime
+timestamp = datetime.now()
+
+import logging
+logging.basicConfig(filename='log_probe_design_{}-{}-{}-{}-{}.txt'.format(timestamp.year, timestamp.month, timestamp.day, timestamp.hour, timestamp.minute), level=logging.NOTSET)
+
+import os
 import time
 import argparse
-import logging
-import pickle
-import copy
 
 import src.utils as utils
-import src.load_exome as le
+import src.load_annotations as la
 import src.get_probes as gp
 import src.filter_probes as fp
-
 
 ############################################
 # pipeline
@@ -30,97 +33,108 @@ def args():
 def probe_pipeline(config):
     
     dir_output = config['dir_output']
+    logging.info('Results will be saved to: {}'.format(dir_output))
     get_annotation = config['get_annotation']
 
     #############
-    ### get exon sequences
+    ### download annotations
     #############
     if get_annotation:
         source = config['source']
-        release_ensemble = config['release_ensemble']
+        logging.info('Download annotation from: {}'.format(source))
         
         if source == 'ncbi':
             # if source ncbi we need a chromosome mapping
-            mapping = le.download_chr_mapping(dir_output)
+            mapping = la.download_chr_mapping(dir_output)
         else:
             mapping = None
         
         if source == 'ensemble':
+            release_ensemble = config['release_ensemble']
             logging.info('Using Ensemble release {}'.format(release_ensemble))
+        else:
+            release_ensemble = None
 
-        logging.info('Download gene annotation from: {}'.format(source))
-        file_gene_annotation = le.download_gene_gtf(dir_output, source, release_ensemble)
-        logging.info('File gene annotation: {}'.format(file_gene_annotation))
+        print('Download gene annotation.')
+        file_gene_gtf = la.download_gene_gtf(source, release_ensemble, mapping, dir_output)
 
-        logging.info('Get exome annotation')
-        file_exon_annotation = le.get_exons_annotation(file_gene_annotation, source, mapping)
-        logging.info('File exon annotation: {}'.format(file_exon_annotation))
-
-        logging.info('Download genome annotation from: {}'.format(source))
-        file_genome_sequence = le.download_genome_fasta(dir_output, source, release_ensemble, mapping)
-        logging.info('File genome sequence: {}'.format(file_genome_sequence))
-
-        logging.info('Get exome fasta')
-        file_exon_sequence = le.get_exons_fasta(file_exon_annotation, file_genome_sequence)
-        logging.info('File exon sequence: {}'.format(file_exon_sequence))
-
+        print('Download genome annotation.')
+        file_genome_fasta = la.download_genome_fasta(source, release_ensemble, mapping, dir_output)
+        
+        logging.info('File gene gtf: {}'.format(file_gene_gtf))
+        logging.info('File genome fasta: {}'.format(file_genome_fasta))
+ 
     else:
-        file_gene_annotation = config['file_gene_annotation']
-        file_exon_annotation = config['file_exon_annotation']
-        file_genome_sequence = config['file_genome_sequence']
-        file_exon_sequence = config['file_exon_sequence']
+        file_gene_gtf = config['file_gene_gtf']
+        file_genome_fasta = config['file_genome_fasta']
 
+    #############
+    ### get gene list
+    #############
+    print('Get gene list.')    
+    exon_annotation = utils.load_exon_annotation(file_gene_gtf)
+
+    if config['gene_list']:
+        genes = config['gene_list']
+    else:
+        genes = utils.get_gene_list(exon_annotation)
+    logging.info('Probes for {} genes will be designed.'.format(len(genes)))
+
+    batch_size = config['batch_size']
+    number_batchs = int(len(genes) / batch_size) + (len(genes) % batch_size > 0)
+    logging.info('{} processes will run in parallel with {} genes in one batch'.format(number_batchs, batch_size))
+
+    print('Get mapping.')
+    t = time.time()
+    utils.get_gene_transcrip_exon_mapping(exon_annotation, dir_output)
+    logging.info('Time to get mapping: {} min'.format((time.time() - t)/60))
+    
 
     #############
     ### get probes
     #############
     probe_length = config['probe_length']
-    min_probes_per_gene = config['min_probes_per_gene']
     GC_content_min = config['GC_content_min']
     GC_content_max = config['GC_content_max']
+    Tm_parameters = config['Tm_parameters']
     Tm_min = config['Tm_min']
     Tm_max = config['Tm_max']
 
-    t = time.time()
-    mapping_exon_to_gene = gp.get_mapping_gene_exon(dir_output, file_gene_annotation, get_annotation)
-    logging.info('Time to process gene-exon mapping dicts: {} s'.format(time.time() - t))
-
-
-    t = time.time()
-    mapping_gene_to_probes = gp.get_mapping_gene_probes(dir_output, file_exon_sequence, mapping_exon_to_gene, probe_length, GC_content_min, GC_content_max, Tm_min, Tm_max, get_annotation)
-    logging.info('Time to process gene-probe mapping dict: {} s'.format(time.time() - t))
-
-
-    t = time.time()
-    mapping_gene_to_probes_unique = gp.get_unique_probes(dir_output, mapping_gene_to_probes, copy.deepcopy(mapping_gene_to_probes), get_annotation)
-    logging.info('Time to process unique probes: {} s'.format(time.time() - t))
-
+    dir_output_probes = os.path.join(dir_output, 'probes/')
+    if os.path.isdir(dir_output_probes) == False:
+        os.mkdir(dir_output_probes)
     
-    removed_genes = []
-    for gene_id, mapping_probe_to_exon in mapping_gene_to_probes_unique.items():
-        if len(mapping_probe_to_exon) < min_probes_per_gene:
-            removed_genes.append(gene_id)
-    logging.info('{} genes were filtered out because they had < {} probes per gene.'.format(len(removed_genes), min_probes_per_gene))
+    print('Get probes.')
+    t = time.time()
+    gp.get_probes(number_batchs, batch_size, exon_annotation, genes, probe_length, GC_content_min, GC_content_max, Tm_parameters, Tm_min, Tm_max, file_genome_fasta, dir_output_probes)
+    logging.info('Time to get probes: {} min'.format((time.time() - t)/60))
 
 
     #############
     ### filter probes
     #############
-    word_size = config['word_size']
+    num_threads_blast = 4
+    word_size = config['word_size'] 
     coverage = config['coverage']
     percent_identity = config['percent_identity']
-    num_threads = int(config['num_threads'])
+    min_probes_per_gene = config['min_probes_per_gene']
 
+
+    print('Run Blast search.')
     t = time.time()
-    mapping_gene_to_probes_blastn = fp.filter_probes_with_blastn(dir_output, file_exon_sequence, mapping_gene_to_probes_unique, mapping_exon_to_gene, min_probes_per_gene, probe_length, word_size, coverage, percent_identity, num_threads)
-    logging.info('Time to process blastn filter: {} s'.format(time.time() - t))
+    fp.run_blast_search(number_batchs, word_size, num_threads_blast, file_gene_gtf, file_genome_fasta, dir_output_probes)
+    logging.info('Time to run Blast search: {} min'.format((time.time() - t)/60))
 
-    #############
-    ### save results
-    #############
-    utils.write_output(dir_output, mapping_gene_to_probes_blastn)
+
+    dir_output_results = os.path.join(dir_output, 'results/')
+    if os.path.isdir(dir_output_results) == False:
+        os.mkdir(dir_output_results)
+
+    print('Filter probes with Blast results')
+    t = time.time()
+    fp.filter_probes_by_blast_results(number_batchs, probe_length, percent_identity, coverage, min_probes_per_gene, dir_output_probes, dir_output_results)
+    logging.info('Time to filter with Blast results: {} min'.format((time.time() - t)/60))
     
-
     
 ############################################
     
@@ -129,8 +143,6 @@ if __name__ == '__main__':
     # get comman line arguments
     parameters = args()
     config = utils.get_config(parameters.config) 
-
-    logging.basicConfig(filename='{}logging.txt'.format(config['dir_output']), level=logging.NOTSET)
     utils.print_config(config, logging)
 
 
@@ -138,6 +150,6 @@ if __name__ == '__main__':
 
     t = time.time()
     probe_pipeline(config)
-    logging.info('Time for whole pipeline: {} s'.format(time.time() - t))
+    logging.info('Time for whole pipeline: {} min'.format((time.time() - t)/60))
 
     logging.info('#########End Pipeline#########')
