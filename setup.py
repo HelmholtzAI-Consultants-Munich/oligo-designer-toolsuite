706a090c5502d2be6d7d88cd1c6ced92c42d9ae7
Lisa Barros de Andrade e Sousa <lisa.barros.andrade.sousa@gmail.com>
2021-09-27 20:20:39 +0200

    update configurations and add environments

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='ProbeDesign',
     version='0.1',
     packages=['src'],
-    install_requires=['argparse','pybedtools','gtfparse','Bio','pandas'],
+    install_requires=['datetime', 'logging', 'os', 'multiprocessing', 'itertools', 'time', 'argparse', 'yaml', 'gzip', 'shutil', 'ftplib', 'pandas', 'pybedtools', 'gtfparse', 'pyfaidx', 'Bio'],
     long_description=open('README.md').read(),
-    author='Lisa B. A. Sousa & Erinc Merdivan',
+    author='Lisa Barros de Andrade e Sousa & Erinc Merdivan',
 )
