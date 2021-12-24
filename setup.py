2412b7c42dcef59e9fb2e0058941beeb6354f4fd
Lisa Barros de Andrade e Sousa <lisa.barros.andrade.sousa@gmail.com>
2021-12-24 01:36:13 +0100

    update package list

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ setup(
     name='ProbeDesign',
     version='0.1',
     packages=['src'],
-    install_requires=['datetime', 'logging', 'os', 'multiprocessing', 'itertools', 'time', 'argparse', 'yaml', 'gzip', 'shutil', 'ftplib', 'pandas', 'pybedtools', 'gtfparse', 'pyfaidx', 'Bio'],
+    install_requires=['os', 'datetime', 'time', 'argparse', 'yaml', 'logging', 
+                      'random', 'pandas', 'gzip', 'shutil', 'ftplib', 
+                      'multiprocessing', 'itertools', 'iteration_utilities', 
+                      'Bio', 'gtfparse', 'pyfaidx', 'pybedtoolsftplib'],
     long_description=open('README.md').read(),
-    author='Lisa Barros de Andrade e Sousa & Erinc Merdivan',
+    author='Lisa Barros de Andrade e Sousa',
 )
