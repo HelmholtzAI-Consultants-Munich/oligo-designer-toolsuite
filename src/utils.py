77c1461de9e3afb7d9287fe4374ee86e84bcd779
Lisa Barros de Andrade e Sousa <lisa.barros.andrade.sousa@gmail.com>
2021-12-24 01:39:15 +0100

    refactor, improve memory and runtime performance

--- src/utils.py
+++ src/utils.py
@@ -8,8 +8,8 @@ import gzip
 import shutil
 
 import pybedtools
-import gtfparse
 
+from ftplib import FTP
 from Bio.SeqUtils import MeltingTemp as mt
 
 ############################################
@@ -17,57 +17,109 @@ from Bio.SeqUtils import MeltingTemp as mt
 ############################################
 
 def get_config(config):
-    """
-    Loads config file in yaml format. 
-    Parameters
-    ----------
-        config: string
-            Path to yaml config file.
-    Returns
-    -------
-        yaml: dict
-            User-defined parameters, where keys are the parameter names and values are the paremeter values.
-    """
+    '''Load config file in yaml format.
+
+    :param config: Path to yaml config file.
+    :type config: string
+    :return: User-defined parameters, where keys are the parameter names and values are the paremeter values.
+    :rtype: dict
+    '''
     with open(config, 'r') as ymlfile:
         return yaml.safe_load(ymlfile)
 
-
-############################################
     
 def print_config(config, logger):
-    """
-    Prints formatted config parameters as <parameter_name>: <parameter_value> to logging file. 
-    Parameters
-    ----------
-        config: string
-            Path to yaml config file.
-        logging: logger
-            Logger object to store important information.
-    Returns
-    -------
-        --- none ---
-    """
+    '''Prints formatted config parameters as <parameter_name>: <parameter_value> to logging file.
+
+    :param config: User-defined parameters, where keys are the parameter names and values are the paremeter values.
+    :type config: dict
+    :param logging: Logger object to store important information.
+    :type logging: logging.basicConfig
+    '''
     logger.info('#########Parameter settings#########')
     for item, value in config.items(): 
         logger.info("{}: {}".format(item, value))
 
-    
-############################################
-    
+
+def create_dir(dir, subdir):
+    '''Create subdirectory and return path to subdirectory.
+
+    :param dir: Path to main directory.
+    :type dir: str
+    :param subdir: Name of subdirectory.
+    :type subdir: str
+    :return: Pyth to subdirectory.
+    :rtype: str
+    '''
+    dir = os.path.join(dir, subdir)
+    if os.path.isdir(dir) == False:
+        os.mkdir(dir)
+
+    return dir
+
+
+def get_Tm_parameters(Tm_parameters):
+    '''Convert config parameters for 'table' attributes of MeltingTemp function into MeltingTemp attributes. 
+
+    :param Tm_parameters: Dictionary with parameters for MeltingTemp function.
+    :type Tm_parameters: dict
+    :return: Dictionary with parameters for MeltingTemp function.
+    :rtype: dict
+    '''
+    Tm_parameters['nn_table'] = getattr(mt, Tm_parameters['nn_table'])
+    Tm_parameters['tmm_table'] = getattr(mt, Tm_parameters['tmm_table'])
+    Tm_parameters['imm_table'] = getattr(mt, Tm_parameters['imm_table'])
+    Tm_parameters['de_table'] = getattr(mt, Tm_parameters['de_table'])
+
+    return Tm_parameters
+
+
+def ftp_download(ftp_link, directory, file_name, dir_output):
+    '''Download file from ftp server.
+
+    :param ftp_link: Link to ftp server, e.g. 'ftp.ncbi.nlm.nih.gov'.
+    :type ftp_link: string
+    :param directory: Directory on ftp server, where the file is located, e.g. 'refseq/H_sapiens/annotation/GRCh38_latest/refseq_identifiers'.
+    :type directory: string
+    :param file_name: Name of file that should be downloaded from ftp server, e.g. 'GCF_000001405.39_GRCh38.p13_genomic.fna'.
+    :type file_name: string
+    :param dir_output: Path to directory for downloaded files.
+    :type dir_output: string
+    :return: Path to downloaded file.
+    :rtype: string
+    '''
+    ftp = FTP(ftp_link)
+    ftp.login()  # login to ftp server
+    ftp.cwd(directory)  # move to directory
+
+    allfiles = [] 
+    ftp.retrlines('LIST ', allfiles.append) 
+
+    for file in allfiles:
+        if file_name in file: 
+            if " -> " in file:  # if file is a symbolic link
+                file_download = file.split(" -> ")[1]
+                raise ValueError('The provided file path is a symbolic link to {}. Please provide the actual path!'.format(file_download))
+                #file_output = file_download.split('/')[-1]
+                #ftp.retrbinary('RETR ' + file_download, open(os.path.join(dir_output, file_output), 'wb').write)
+            else:
+                file_download = file.split(" ")[-1]
+                file_output = os.path.join(dir_output, file_download)
+                ftp.retrbinary('RETR ' + file_download, open(file_output, 'wb').write)
+
+    ftp.quit()
+
+    return file_output
+
+
 def decompress_gzip(file_gzip):
-    """
-    Decompress zip files.
-    Parameters
-    ----------
-        file_gzip: string
-            Path to zipped file.
-            
-    Returns
-    -------
-        file_output: string
-            Path to unzipped file.
-            
-    """
+    '''Decompress zip files.
+
+    :param file_gzip: Path to zipped file.
+    :type file_gzip: string
+    :return: Path to unzipped file.
+    :rtype: string
+    '''
     file_output = file_gzip.split('.gz')[0]
     with gzip.open(file_gzip, 'rb') as f_in:
         with open(file_output, 'wb') as f_out:
@@ -75,113 +127,36 @@ def decompress_gzip(file_gzip):
     return file_output
 
 
-############################################
-
-def load_exon_annotation(file_gene_gtf):
-    """
-    Load exon annotation from gtf file. 
-    Parameters
-    ----------
-        file_gene_gtf: string
-            Path to gtf file with gene annotation.
-    Returns
-    -------
-        exon_annotation: pandas.DataFrame
-            Dataframe with exon annotation.
-            
-    """
-    gene_annotation = gtfparse.read_gtf(file_gene_gtf)
-    exon_annotation = gene_annotation.loc[gene_annotation['feature'] == 'exon']
-    exon_annotation = exon_annotation.assign(source='unknown')
-    if not 'exon_id' in exon_annotation.columns:
-        exon_annotation['exon_id'] = exon_annotation['transcript_id'] + '_exon' + exon_annotation['exon_number']
-
-    return exon_annotation
+def merge(file_bed):
+    annotation = pybedtools.BedTool(file_bed)
+    annotation = annotation.merge(d=0, s=True, )
 
 
-############################################
 
-def load_transcriptome_annotation(file_gene_gtf):
-    """
-    Load transcriptome annotation from gtf file. 
-    Parameters
-    ----------
-        file_gene_gtf: string
-            Path to gtf file with gene annotation.
-    Returns
-    -------
-        transcriptome_annotation: pandas.DataFrame
-            Dataframe with transcript annotation.
-    """
-    gene_annotation = gtfparse.read_gtf(file_gene_gtf)
-    transcriptome_annotation = gene_annotation.loc[gene_annotation['feature'] == 'transcript']
-    transcriptome_annotation = transcriptome_annotation.assign(source='unknown')
+def get_fasta(file_bed, file_reference_fasta, file_fasta, split=False):
+    '''Get sequence for regions annotated in input gft file using genome fasta file.
 
-    return transcriptome_annotation
+    :param file_bed: Path to bed file with annotated genomic regions.
+    :type file_bed: string
+    :param file_reference_fasta: Path to fasta file with reference sequence, e.g. transcriptome.
+    :type file_reference_fasta: string
+    :param file_fasta: Path to fasta file where retrieved sequences are written to.
+    :type file_fasta: string
+    :param split: Use -split option of bedtools getfasta, defaults to False
+    :type split: bool
+    '''
+    annotation = pybedtools.BedTool(file_bed)
+    genome_sequence = pybedtools.BedTool(file_reference_fasta)
 
-
-############################################
-
-def get_fasta(file_gtf, file_genome_fasta, file_fasta):
-    """
-    Get sequence for regions annotated in input gft file using genome fasta file. 
-    Parameters
-    ----------
-        file_gtf: string
-            Path to gtf file with annotated genomic regions.
-        file_genome_fasta: string
-            Path to fasta file with genome sequence.
-        file_fasta: string
-            Path to fasta file where retrieved sequences are written to.
-    Returns
-    -------
-        --- none ---
-    """
-    annotation = pybedtools.BedTool(file_gtf)
-    genome_sequence = pybedtools.BedTool(file_genome_fasta)
-
-    annotation = annotation.sequence(fi=genome_sequence, s=True, name=True)
+    annotation = annotation.sequence(fi=genome_sequence, s=True, name=True, split=split)
     annotation.save_seqs(file_fasta)
 
 
-############################################
-
-def read_gene_list(file_genes):
-    """
-    Read list of genes from text file. Gene names must be provided in ensemble or NCBI annotation format. 
-    Parameters
-    ----------
-        file_genes: string
-            Path to text file with gene names.
-    Returns
-    -------
-        genes: list
-            List of gene names.
-    """
-    with open(file_genes) as file:
-        lines = file.readlines()
-        genes = [line.rstrip() for line in lines]
-    return genes
 
 
-############################################
 
-def get_Tm_parameters(Tm_parameters):
-    """
-    Convert config parameters for 'table' attributes of MeltingTemp function into MeltingTemp attributes. 
-    Parameters
-    ----------
-        Tm_parameters: dict
-            Dictionary with parameters for MeltingTemp function.
-    Returns
-    -------
-        Tm_parameters: dict
-            Dictionary with parameters for MeltingTemp function.
-    """
-    # get the attributed for the parameter
-    Tm_parameters['nn_table'] = getattr(mt, Tm_parameters['nn_table'])
-    Tm_parameters['tmm_table'] = getattr(mt, Tm_parameters['tmm_table'])
-    Tm_parameters['imm_table'] = getattr(mt, Tm_parameters['imm_table'])
-    Tm_parameters['de_table'] = getattr(mt, Tm_parameters['de_table'])
 
-    return Tm_parameters
+
+
+
+
