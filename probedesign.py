9e40ae94e2120bc8b3f0fff000155e98877896d4
LouisK92 <louiskuemmerle@googlemail.com>
2022-03-18 18:31:34 +0100

    Add search for best nonoverlapping sets

--- probedesign.py
+++ probedesign.py
@@ -14,6 +14,7 @@ logging.basicConfig(filename='log_probe_design_{}-{}-{}-{}-{}.txt'.format(timest
 from src.utils import get_config, print_config, rm_intermediate_files
 from src.datamodule import DataModule
 from src.probefilter import ProbeFilter
+from src.nonoverlapping_sets import get_nonoverlapping_sets
 
 
 ############################################
@@ -80,6 +81,16 @@ def probe_pipeline(config, dir_output):
     print('Time to filter with Blast results: {} min \n'.format(t))
 
     probefilter.log_statistics()
+    
+    t = time.time()
+    get_nonoverlapping_sets(config,os.path.join(dir_output,"probes"),os.path.join(dir_output,"overlap"),
+                            os.path.join(dir_output,"probesets"),n_sets=100
+                           )
+    t = (time.time() - t)/60
+
+    logging.info('Time to find nonoverlapping probe sets: {} min'.format(t))
+    print('Time to find nonoverlapping probe sets: {} min \n'.format(t))
+    
 
 
 ############################################
