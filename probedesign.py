aa62d770993220d3e9738b6f20531e342bec83da
LouisK92 <louiskuemmerle@googlemail.com>
2022-03-18 16:45:29 +0100

    Add --download_only flag to pipeline and check if files are already downloaded

--- probedesign.py
+++ probedesign.py
@@ -7,6 +7,7 @@ import time
 import argparse
 import logging
 from datetime import datetime
+from pathlib import Path
 
 timestamp = datetime.now()
 logging.basicConfig(filename='log_probe_design_{}-{}-{}-{}-{}.txt'.format(timestamp.year, timestamp.month, timestamp.day, timestamp.hour, timestamp.minute), level=logging.NOTSET)
@@ -29,9 +30,11 @@ def args():
     args_parser = argparse.ArgumentParser(description=__doc__,formatter_class=argparse.RawDescriptionHelpFormatter)
     args_parser.add_argument('-c','--config',help='path to config yaml file',type=str,required=True)
     args_parser.add_argument('-o','--output',help='path of output folder',type=str,required=True)
+    args_parser.add_argument('-d','--download_only',help='dry run that only downloads the necessary files',
+                             action='store_true', required=False)
     return args_parser.parse_args()
 
-def probe_pipeline(config, dir_output):
+def probe_pipeline(config, dir_output, download_only=False):
     '''Pipeline of probe designer. Sets up all required directories; loads annotations, genes and probes; 
     and filters probes based on sequence properties and blast aligment search results.
 
@@ -39,14 +42,20 @@ def probe_pipeline(config, dir_output):
     :type config: dict
     :param dir_output: User-defined output directory.
     :type dir_output: string
+    :param download_only: Whether to interupt the pipeline after download of the necessary files.
+    :type download_only: bool
     '''
     dir_output = os.path.join(dir_output, "")
+    Path(dir_output).mkdir(parents=True, exist_ok=True)
     logging.info('Results will be saved to: {}'.format(dir_output))
 
     datamodule = DataModule(config, logging, dir_output)
     
     t = time.time()
     datamodule.load_annotations()
+    if download_only:
+        logging.info("Download finished. Pipeline interrupted. Set download_only to False to run the full pipeline.")
+        return
     datamodule.load_genes()
     datamodule.load_transcriptome()
     datamodule.load_probes()
@@ -94,13 +103,14 @@ if __name__ == '__main__':
 
     dir_output = parameters.output
     config = get_config(parameters.config)
+    download_only = parameters.download_only
     print_config(config, logging)
 
     logging.info('#########Start Pipeline#########')
     print('#########Start Pipeline#########')
 
     t_pipeline = time.time()
-    probe_pipeline(config, dir_output)
+    probe_pipeline(config, dir_output, download_only=download_only)
     t_pipeline = (time.time() - t_pipeline)/60
 
     logging.info('Time Pipeline: {} min'.format(t_pipeline))
