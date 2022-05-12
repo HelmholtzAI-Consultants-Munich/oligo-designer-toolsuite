e753e854d1d379e09596f25bb49c2838a9622f4f
HAICU Mekki <isra.mekki@icb-lisa.scidom.de>
2022-05-12 04:48:46 +0200

    Add wrapper to pipeline steps

--- oligo_designer_toolsuite/probedesign.py
+++ oligo_designer_toolsuite/probedesign.py
@@ -2,22 +2,15 @@
 # imports
 ############################################
 
-import os
 import time
 import argparse
 import logging
 from datetime import datetime
-from pathlib import Path
 
 timestamp = datetime.now()
 logging.basicConfig(filename='log_probe_design_{}-{}-{}-{}-{}.txt'.format(timestamp.year, timestamp.month, timestamp.day, timestamp.hour, timestamp.minute), level=logging.NOTSET)
 
-from oligo_designer_toolsuite.utils import get_config, print_config, rm_intermediate_files
-from oligo_designer_toolsuite.datamodule import DataModule
-from oligo_designer_toolsuite.probefilter import ProbeFilter
-from oligo_designer_toolsuite.get_overlap_matrix import get_overlap_matrix
-from oligo_designer_toolsuite.nonoverlapping_sets import get_nonoverlapping_sets
-from oligo_designer_toolsuite.padlock_design import design_padlocks
+from oligo_designer_toolsuite.pipeline import download_and_filter, find_non_overlapping_sets, design_padlock_probes
 
 
 
@@ -37,93 +30,7 @@ def args():
     args_parser.add_argument('-d','--download_only',help='dry run that only downloads the necessary files',action='store_true',required=False)
     return args_parser.parse_args()
 
-def probe_pipeline(config, dir_output, download_only=False):
-    '''Pipeline of probe designer. Sets up all required directories; loads annotations, genes and probes; 
-    and filters probes based on sequence properties and blast aligment search results.
-
-    :param config: User-defined parameters, where keys are the parameter names and values are the paremeter values.
-    :type config: dict
-    :param dir_output: User-defined output directory.
-    :type dir_output: string
-    :param download_only: Whether to interupt the pipeline after download of the necessary files.
-    :type download_only: bool
-    '''
-    dir_output = os.path.join(dir_output, "")
-    Path(dir_output).mkdir(parents=True, exist_ok=True)
-    logging.info('Results will be saved to: {}'.format(dir_output))
-
-    datamodule = DataModule(config, logging, dir_output)
-    
-    t = time.time()
-    datamodule.load_annotations()
-    if download_only:
-        logging.info("Download finished. Pipeline interrupted. Set download_only to False to run the full pipeline.")
-        return
-    datamodule.load_genes()
-    datamodule.load_transcriptome()
-    datamodule.load_probes()
-    t = (time.time() - t)/60
-
-    probefilter = ProbeFilter(config, logging, dir_output, datamodule.file_transcriptome_fasta, datamodule.genes)
-    del datamodule # free memory
-
-    logging.info('Time to load annotations, genes, transcriptome and probes: {} min'.format(t))
-    print('Time to load annotations, genes, transcriptome and probes: {} min \n'.format(t))
-
-    t = time.time()
-    probefilter.filter_probes_by_exactmatch()
-    t = (time.time() - t)/60
-
-    logging.info('Time to filter with extact matches: {} min'.format(t))
-    print('Time to filter with extact matches: {} min \n'.format(t))
-
-    t = time.time()
-    probefilter.run_blast_search()
-    t = (time.time() - t)/60
-
-    logging.info('Time to run Blast search: {} min'.format(t))
-    print('Time to run Blast search: {} min \n'.format(t))
-
-    t = time.time()
-    probefilter.filter_probes_by_blast_results()
-    t = (time.time() - t)/60
-
-    logging.info('Time to filter with Blast results: {} min'.format(t))
-    print('Time to filter with Blast results: {} min \n'.format(t))
-
-    probefilter.log_statistics()
-    
-    t = time.time()
-
-    get_overlap_matrix(os.path.join(dir_output,"probes"),os.path.join(dir_output,"overlap"))
-    t = (time.time() - t)/60    
-    
-    logging.info('Time to compute overlap matrices: {} min'.format(t))
-    print('Time to compute overlap matrices: {} min \n'.format(t))
-    
-    t = time.time()
-
-    get_nonoverlapping_sets(config,os.path.join(dir_output,"probes"),os.path.join(dir_output,"overlap"),
-                            os.path.join(dir_output,"probesets"),n_sets=100)
-    t = (time.time() - t)/60
-
-    logging.info('Time to find nonoverlapping probe sets: {} min'.format(t))
-    print('Time to find nonoverlapping probe sets: {} min \n'.format(t))
-    
-    t = time.time()    
-    design_padlocks(config,os.path.join(dir_output,"probes"),os.path.join(dir_output,"probesets"),
-                    os.path.join(dir_output,"padlock_probes"))
-    t = (time.time() - t)/60
-
-    logging.info('Time to design padlock probes: {} min'.format(t))
-    print('Time to design padlock probes: {} min \n'.format(t))    
-
-
-
-############################################
-# main
-############################################
-    
+  
 def main():
     '''Main function of probe designer.
     '''
@@ -139,7 +46,11 @@ def main():
     print('#########Start Pipeline#########')
 
     t_pipeline = time.time()
-    probe_pipeline(config, dir_output, download_only=download_only)
+    
+    pipeline.download_and_filter(config, dir_output, logging, download_only=download_only)
+    pipeline.find_non_overlapping_sets(config, dir_output, logging)
+    pipeline.design_padlock_probes(config, dir_output, logging)
+    
     t_pipeline = (time.time() - t_pipeline)/60
 
     logging.info('Time Pipeline: {} min'.format(t_pipeline))
