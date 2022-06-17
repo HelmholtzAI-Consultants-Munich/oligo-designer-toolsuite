f6bfe660206787aef7d2fa4401ba98a0b3367a44
Chelsea <chels.alex.bright@gmail.com>
2022-06-17 16:34:37 +0200

    run pre-commit

--- oligo_designer_toolsuite/IO/_data_parser.py
+++ oligo_designer_toolsuite/IO/_data_parser.py
@@ -1,4 +1,3 @@
-
 import os
 import shutil
 
@@ -6,24 +5,28 @@ import shutil
 def read_gtf():
     pass
 
+
 def get_sequence_from_annotation():
     pass
+
 
 def check_gtf_format():
     pass
 
+
 def check_fasta_format():
     pass
+
 
 def merge_fasta(files_fasta, file_merged_fasta):
 
     if files_fasta == []:
-        raise ValueError('No fasta files provided for merge.')
+        raise ValueError("No fasta files provided for merge.")
 
-    with open(file_merged_fasta, 'w') as handle_DB:
+    with open(file_merged_fasta, "w") as handle_DB:
         for file in files_fasta:
             if os.path.exists(file):
-                shutil.copyfileobj(open(file,'rb'), handle_DB)
+                shutil.copyfileobj(open(file, "rb"), handle_DB)
                 shutil.rmtree(file)
             else:
-                raise ValueError(f'Fasta file {file} does not exist!')
+                raise ValueError(f"Fasta file {file} does not exist!")
