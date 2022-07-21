c3854f21dbac3ae3fb479b0953a328427bde9d09
Chelsea <chels.alex.bright@gmail.com>
2022-07-21 12:21:19 +0200

    test new multiprocessing

--- oligo_designer_toolsuite/oligo_specificity_filter/_filter_base.py
+++ oligo_designer_toolsuite/oligo_specificity_filter/_filter_base.py
@@ -1,17 +1,16 @@
 import logging
 import os
-import warnings
+from abc import abstractmethod
 from pathlib import Path
-
-import pandas as pd
 
 
 class ProbeFilterBase:
+
     """This is the base class for all filter classes
 
-    :param no_batches: How many sub-processes to use
-    :type no_batches: int
-    :param ligation_region: If ligation region present
+    :param number_batches: How many batches of files to split data into for filtering
+    :type number_batches: int
+    :param ligation_region: If ligation region is present
     :type ligation_region: bool"""
 
     def __init__(
@@ -21,20 +20,55 @@ class ProbeFilterBase:
         dir_output,
         file_probe_info,
         genes,
+        n_jobs=None,
         dir_annotations=None,
-        number_subbatches=1,
+        number_subbatches=None,
         max_genes_in_batch=300,
     ):
-        self.number_batches = (number_batches,)
+        """This is the base class for all filter classes
+
+        :param number_batches: How many batches of files to split data into for filtering
+        :type number_batches: int
+        :param ligation_region: If ligation region is present
+        :type ligation_region: bool
+        :param dir_output: output directory to write file containing filtered probes to
+        :type dir_output: str
+        :param file_probe_info: gtf file containing probe info
+        :type file_probe_info: str
+        :param genes: list of genes given by user
+        :type genes: list
+        :param n_jobs: number of processes to run in parallel when filtering probes, defaults to number_batches
+        :type n_jobs: int, optional
+        :param dir_annotations: directory storing file annotations of probes, defaults to None
+        :type dir_annotations: str, optional
+        :param number_subbatches: number of subbatches to split batched data into to control how many genes are in one batch, defaults to None
+        :type dir_annotations: int, optional
+        :param max_genes_in_batch: max number of genes allowed in a batch, defaults to 300
+        :type max_genes_in_batch: int, optional
+        """
+        self.number_batches = number_batches
         self.ligation_region = ligation_region
         self.dir_output = dir_output
-        self.number_subbatches = number_subbatches  # if number of genes in batch > max_genes_in_batch then split batch into multiple subbatches
         self.max_genes_in_batch = max_genes_in_batch  # if more than 300 genes in one batch split into subbatches to reduce required memory for loading blast results
         self.file_probe_info = file_probe_info
         self.genes = genes
         self.removed_genes = genes
 
+        if number_subbatches == None:
+            self.number_subbatches = (
+                len(genes) // self.max_genes_in_batch
+            ) + 1  # if number of genes in batch > max_genes_in_batch then split batch into multiple subbatches
+        else:
+            self.number_subbatches = number_subbatches
+
+        if n_jobs == None:
+            self.n_jobs = self.number_batches
+        else:
+            self.n_jobs = n_jobs
+
         # set logger
+        log_file = os.path.join(self.dir_output, "logs/filter_probes.log")
+        logging.basicConfig(filename=log_file, filemode="w", level=logging.INFO)
         self.logging = logging.getLogger("filter_probes")
 
         # set directory
@@ -44,43 +78,6 @@ class ProbeFilterBase:
         else:
             self.dir_annotations = dir_annotations
 
-    def create_batches(self, number_batches):
-
-        probeinfo_tsv = pd.read_csv(self.file_probe_info, sep="\t", header=0)
-
-        batch_size = int(len(self.genes) / number_batches) + (
-            len(self.genes) % number_batches > 0
-        )
-
-        for batch_id in range(number_batches):
-
-            file_probe_info_batch = os.path.join(
-                self.dir_annotations, "probes_info_batch{}.txt".format(batch_id)
-            )
-            # file_region_fasta_batch = os.path.join(self.dir_annotations, "region_batch{}.fna".format(batch_id))
-            file_probe_sequence_batch = os.path.join(
-                self.dir_annotations, "probes_sequence_batch{}.txt".format(batch_id)
-            )
-
-            # batch probe info
-            genes_batch = self.genes[
-                (batch_size * batch_id) : (
-                    min(batch_size * (batch_id + 1), len(self.genes) + 1)
-                )
-            ]
-
-            probeinfo = probeinfo_tsv.loc[
-                probeinfo_tsv["gene_id"].isin(genes_batch)
-            ].copy()
-            probeinfo.reset_index(inplace=True, drop=True)
-            probeinfo.to_csv(file_probe_info_batch, sep="\t", header=True, index=False)
-
-            sequences = probeinfo[probeinfo.columns[1]]
-            with open(file_probe_sequence_batch, "w") as handle_out:
-                for seq in sequences:
-                    handle_out.write(seq + "\n")
-
-    def apply_base(self):
-        """Apply filter to file_transcriptome_fasta and save output in dir_output"""
-
-        warnings.warn(f"No apply function  for {type(self).__name__}")
+    @abstractmethod
+    def apply(self):
+        """Apply filter to list of all possible probes given user-specified genes and save output in dir_output"""
--- oligo_designer_toolsuite/oligo_specificity_filter/_filter_blastn.py
+++ oligo_designer_toolsuite/oligo_specificity_filter/_filter_blastn.py
@@ -12,10 +12,6 @@ from ._filter_base import ProbeFilterBase
 
 
 class ProbeFilterBlastn(ProbeFilterBase):
-    """This class filters probes based on the blast alignment tool.
-
-    :param"""
-
     def __init__(
         self,
         number_batches,
@@ -24,22 +20,39 @@ class ProbeFilterBlastn(ProbeFilterBase):
         dir_output,
         file_probe_info,
         genes,
+        n_jobs,
         word_size,
         percent_identity,
+        coverage,
         probe_length_min,
         probe_length_max,
-        coverage,
     ):
+        """This class filters probes based on the blast alignment tool
+
+        :param file_transcriptome_fasta: path to fasta file containing all probes
+        :type file_transcriptome_fasta: str
+        :param word_size: #word size for the blastn seed (exact match to target)
+        :type word_size: int
+        :param percent_identity: maximum similarity between probes and target sequences, ranging from 0 to 100% (no missmatch)
+        :type percent_identity: int
+        :param coverage: minimum coverage between probes and target sequence, ranging from 0 to 100% (full coverage)
+        :type coverage: int
+        :param probe_length_min: minimum length of the probes created
+        :type probe_length_min: int
+        :param probe_length_max: maximum length of the probes created
+        :type probe_length_max: int
+
+        """
         super().__init__(
-            number_batches, ligation_region, dir_output, file_probe_info, genes
+            number_batches, ligation_region, dir_output, file_probe_info, genes, n_jobs
         )
         self.number_batches = number_batches
         self.word_size = word_size
         self.percent_identity = percent_identity
-        self.probe_length_min = probe_length_min
-        self.probe_length_max = probe_length_max
         self.coverage = coverage
         self.file_transcriptome_fasta = file_transcriptome_fasta
+        self.probe_length_min = probe_length_min
+        self.probe_length_max = probe_length_max
 
         self.dir_blast = os.path.join(self.dir_output, "blast")
         Path(self.dir_blast).mkdir(parents=True, exist_ok=True)
@@ -51,13 +64,13 @@ class ProbeFilterBlastn(ProbeFilterBase):
             self.dir_output, "genes_with_insufficient_probes.txt"
         )
 
-    def run_blast_search(self, num_threads):
+    def run_blast_search(self):
         """Run BlastN alignment tool to find regions of local similarity between sequences, where sequences are probes and transcripts.
         BlastN identifies the transcript regions where probes match with a certain coverage and similarity.
         """
 
         def _run_blast(batch_id):
-            """Run BlastN alignment search for all probes of one batch.
+            """Run BlastN alignment search for all probes of one batch
             :param batch_id: Batch ID.
             :type batch_id: int
             """
@@ -78,7 +91,7 @@ class ProbeFilterBlastn(ProbeFilterBase):
                     strand="plus",
                     word_size=self.word_size,
                     perc_identity=self.percent_identity,
-                    num_threads=num_threads,
+                    num_threads=1,
                 )
                 out, err = cmd()
 
@@ -96,16 +109,20 @@ class ProbeFilterBlastn(ProbeFilterBase):
         # run blast in parallel
         start_time = time.perf_counter()
         with parallel_backend("loky"):
-            Parallel(n_jobs=2)(
-                delayed(self._run_blast)(batch_id)
+            """Parallel(n_jobs=self.n_jobs)(
+                delayed(_run_blast)(batch_id)
                 for batch_id in range(self.number_batches)
+            )"""
+            Parallel(n_jobs=self.n_jobs, prefer="threads")(
+                delayed(_run_blast)(batch_id) for batch_id in range(self.number_batches)
             )
-            # Parallel(n_jobs=3, prefer="threads")(delayed(self._filter_probes_exactmatch)(batch_id) for batch_id in range(self.number_batches))
         finish_time = time.perf_counter()
-        self.logging.info(f"Blast run finished in {finish_time-start_time} seconds")
+        self.logging.info(
+            f"Blast alignmnet search finished in {finish_time-start_time} seconds"
+        )
 
     def filter_probes_by_blast_results(self):
-        """Process the output of the BlastN alignment search.
+        """Process the output of the BlastN alignment search
         :param batch_id: Batch ID.
         :type batch_id: int
         """
@@ -120,7 +137,7 @@ class ProbeFilterBlastn(ProbeFilterBase):
                 num_probes_wo_match += _filter_probes_blast(probes_info, blast_results)
 
         def _load_probes_info(batch_id):
-            """Load filtered probe infomration from tsv file.
+            """Load filtered probe information from tsv file
             :param batch_id: Batch ID.
             :type batch_id: int
             :return: Dataframe with probe information, filtered based on sequence properties.
@@ -144,7 +161,7 @@ class ProbeFilterBlastn(ProbeFilterBase):
                     "strand": str,
                     "GC_content": float,
                     "melting_temperature": float,
-                    "melt_temp_rm1": float,
+                    "melt_temp_arm1": float,
                     "melt_temp_arm2": float,
                     "dif_melt_temp_arms": float,
                     "ligation_site": int,
@@ -204,7 +221,6 @@ class ProbeFilterBlastn(ProbeFilterBase):
             :param blast_results: DataFrame with processed blast alignment search results.
             :type blast_results: pandas.DataFrame
             """
-            # blast_results_matches = blast_results[~(blast_results[['query_gene_id','target_gene_id']].nunique(axis=1) == 1)]
             blast_results_matches = blast_results[
                 blast_results["query_gene_id"] != blast_results["target_gene_id"]
             ]
@@ -292,6 +308,7 @@ class ProbeFilterBlastn(ProbeFilterBase):
 
         def _write_removed_genes():
             """Write list of genes for which not enough probes could be designed for."""
+
             # create file where removed genes are saved
             _, _, probe_files = next(os.walk(self.dir_probes))
             for probe_file in probe_files:
@@ -305,13 +322,15 @@ class ProbeFilterBlastn(ProbeFilterBase):
 
         start_time = time.perf_counter()
         with parallel_backend("loky"):
-            Parallel(n_jobs=2)(
-                delayed(self._process_blast_results)(batch_id)
+            """Parallel(n_jobs=self.n_jobs)(
+                delayed(_process_blast_results)(batch_id)
+                for batch_id in range(self.number_batches)
+            )"""
+            Parallel(n_jobs=self.n_jobs, prefer="threads")(
+                delayed(_process_blast_results)(batch_id)
                 for batch_id in range(self.number_batches)
             )
-            # Parallel(n_jobs=3, prefer="threads")(delayed(self._filter_probes_exactmatch)(batch_id) for batch_id in range(self.number_batches))
         finish_time = time.perf_counter()
-        self.logging.info(f"Blast run finished in {finish_time-start_time} seconds")
 
         self.logging.info(
             f"Blast results processed in {finish_time-start_time} seconds"
