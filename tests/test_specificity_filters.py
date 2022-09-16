a9fcd2414da3777a2faea37a9c64c2368201d0c6
Chelsea <chels.alex.bright@gmail.com>
2022-09-16 09:29:00 +0200

    add tests for specificity filters using pytest

--- tests/test_specificity_filters.py
+++ tests/test_specificity_filters.py
@@ -12,21 +12,28 @@ from oligo_designer_toolsuite.oligo_specificity_filter._filter_blastn import (
 from oligo_designer_toolsuite.oligo_specificity_filter._filter_bowtie import (
     ProbeFilterBowtie,
 )
+from oligo_designer_toolsuite.oligo_specificity_filter._filter_bowtie2 import (
+    ProbeFilterBowtie2,
+)
 
 cwd = os.getcwd()
 
-n_jobs = 10
+n_jobs = 1
 ligation_region = 0
-dir_output = cwd + "/output/"
-dir_data = cwd + "/data/"
+dir_output = cwd + "/output"
+dir_annotations = cwd + "/data"
 file_probe_info = (
-    dir_data + "oligo_DB_unknown_unknown_Custom_release_unknown_gene_transcript.tsv"
+    dir_annotations
+    + "/oligo_DB_unknown_unknown_Custom_release_unknown_gene_transcript.tsv"
 )
 min_probes_per_gene = 2
 file_transcriptome_fasta = (
-    dir_data
-    + "reference_DB_unknown_unknown_Custom_release_unknown_genome_False_gene_transcript_True"
+    dir_annotations
+    + "/reference_DB_unknown_unknown_Custom_release_unknown_genome_False_gene_transcript_True"
 )
+
+file_probe_info_match = dir_annotations + "/oligo_DB_match.tsv"
+file_probe_info_no_match = dir_annotations + "/oligo_DB_no_match.tsv"
 word_size = 10
 percent_identity = 80
 probe_length_min = 30
@@ -35,6 +42,8 @@ coverage = 50
 min_mismatches = 4
 mismatch_region = 5
 
+probe_info_dict_match = read_oligos_DB_tsv(file_probe_info_match)
+probe_info_dict_no_match = read_oligos_DB_tsv(file_probe_info_no_match)
 probe_info_dict = read_oligos_DB_tsv(file_probe_info)
 
 
@@ -44,33 +53,71 @@ def test_filter_bowtie_format():
     bowtie_filter = ProbeFilterBowtie(
         n_jobs,
         dir_output,
-        probe_length_min,
-        probe_length_max,
+        dir_annotations,
         file_transcriptome_fasta,
         min_mismatches,
         mismatch_region,
+        ligation_region,
     )
 
-    bowtie_filter.apply(probe_info_dict)
+    bowtie_filter.apply(probe_info_dict_no_match)
 
-    df_correct_format = pd.read_csv(file_probe_info)
+    df_correct_format = pd.read_csv(file_probe_info_no_match)
 
-    bowtie_sample_output = pd.read_csv(cwd + "/probes_bowtie/probes_AGRN.txt")
+    blast_sample_output = pd.read_csv(dir_output + "/probes_bowtie/probes_AGRN.txt")
+    assert list(df_correct_format.columns) == list(blast_sample_output.columns)
+
+
+def test_filter_bowtie_all_matches():
+    # Check that bowtie filter filters out a sequence that is identified as a match for user-defined threshholds
+
+    # Run blast filter
+    bowtie_filter = ProbeFilterBowtie(
+        n_jobs,
+        dir_output,
+        dir_annotations,
+        file_transcriptome_fasta,
+        min_mismatches,
+        mismatch_region,
+        ligation_region,
+    )
+
+    bowtie_filter.apply(probe_info_dict_match)
+
+    # Check that gene of matching probe is added to file genes_with_insufficient_probes.txt
+    blast_sample_output = pd.read_csv(
+        dir_output + "/probes_bowtie/genes_with_insufficient_probes.txt",
+        sep="\t",
+        header=None,
+    )
+    assert not blast_sample_output.empty
+
+
+def test_filter_bowtie2_format():
+
+    # Run Bowtie2 filter
+    bowtie2_filter = ProbeFilterBowtie2(
+        n_jobs, dir_output, dir_annotations, file_transcriptome_fasta
+    )
+
+    bowtie2_filter.apply(probe_info_dict_no_match)
+
+    df_correct_format = pd.read_csv(probe_info_dict_no_match)
+
+    bowtie_sample_output = pd.read_csv(cwd + "/probes_bowtie2/probes_WASH7P.txt")
     assert (
         df_correct_format.columns().tolist() == bowtie_sample_output.columns().tolist()
     )
 
-    # Give single sequence to filters
-    # 1. Seq that is expected to be filtered out (Similar match)
-
 
 def test_filter_blast_format():
+    # Check that filter outputs correct file format
 
-    # Run Bowtie filter
     blast_filter = ProbeFilterBlastn(
         n_jobs,
         file_transcriptome_fasta,
         dir_output,
+        dir_annotations,
         word_size,
         percent_identity,
         coverage,
@@ -79,14 +126,40 @@ def test_filter_blast_format():
         ligation_region,
     )
 
-    blast_filter.apply(probe_info_dict)
+    blast_filter.apply(probe_info_dict_no_match)
 
-    df_correct_format = pd.read_csv(file_probe_info)
+    df_correct_format = pd.read_csv(file_probe_info_no_match)
 
     blast_sample_output = pd.read_csv(dir_output + "/probes_blast/probes_AGRN.txt")
-    assert (
-        df_correct_format.columns().tolist() == blast_sample_output.columns().tolist()
+    assert list(df_correct_format.columns) == list(blast_sample_output.columns)
+
+
+def test_filter_blast_all_matches():
+    # Check that blast filter filters out a sequence that is identified as a match for user-defined threshholds
+
+    # Run blast filter
+    blast_filter = ProbeFilterBlastn(
+        n_jobs,
+        file_transcriptome_fasta,
+        dir_output,
+        dir_annotations,
+        word_size,
+        percent_identity,
+        coverage,
+        probe_length_min,
+        probe_length_max,
+        ligation_region,
     )
+
+    blast_filter.apply(probe_info_dict_match)
+
+    # Check that gene of matching probe is added to file genes_with_insufficient_probes.txt
+    blast_sample_output = pd.read_csv(
+        dir_output + "/probes_blast/genes_with_insufficient_probes.txt",
+        sep="\t",
+        header=None,
+    )
+    assert not blast_sample_output.empty
 
 
 # TODO: Check if ligation filter works for all filters
