07dbbf2d86b908f51eca9387945bbce4bd9dbef5
Isra Mekki <98828872+IsraMekki0@users.noreply.github.com>
2022-12-21 10:53:30 +0100

    Update __init__.py

--- oligo_designer_toolsuite/IO/__init__.py
+++ oligo_designer_toolsuite/IO/__init__.py
@@ -1,6 +1,6 @@
-"""
+'''
 Manages the databases required for the oligos generations. In particular, it deals with the input and output of data.
-"""
+'''
 
 
 from ._database import CustomDB, EnsemblDB, NcbiDB
