163a69485b2f9c36b9c0ddf8051200096d36bc55
Lisa B. A. Sousa <lisa.barros.andrade.sousa@gmail.com>
2023-01-20 14:48:29 +0100

    include notebooks in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,6 +37,7 @@ extensions = [
     "sphinx.ext.todo",
     "sphinx.ext.autosummary",
     "sphinx.ext.napoleon",
+    "myst_nb",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
