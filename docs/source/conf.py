0736acda39064b011c29bebc317aa09f0d8d7f01
Lisa B. A. Sousa <lisa.barros.andrade.sousa@gmail.com>
2023-01-23 10:43:19 +0100

    bugfix conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ import sys
 from pathlib import Path
 
 # Add parent dir to known paths
-sys.path.insert(0, os.path.abspath('../../oligo_designer_toolsuite'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- Project information -----------------------------------------------------
