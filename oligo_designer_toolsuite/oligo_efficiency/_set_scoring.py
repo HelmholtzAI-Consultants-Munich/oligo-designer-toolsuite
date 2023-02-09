a3a937c22207d491d45f3ba4e9072f20bff462bd
Lisa B. A. Sousa <lisa.barros.andrade.sousa@gmail.com>
2023-02-09 15:30:00 +0100

    adjust moduels to refactored database

--- oligo_designer_toolsuite/oligo_efficiency/_set_scoring.py
+++ oligo_designer_toolsuite/oligo_efficiency/_set_scoring.py
@@ -4,18 +4,18 @@ import pandas as pd
 
 
 class SetScoringBase(ABC):
-    """Template class for scoring the stest of oligos."""
+    """Template class for scoring the sets of oligos."""
 
     @abstractmethod
-    def apply(self, clique_oligos: pd.Series, n: int):
-        """From a set of non-overlapping oligos extracts the best subset of n oligos and its score. Recieves in input
+    def apply(self, oligo_set: pd.Series, n: int):
+        """From a set of oligos extracts the best subset of n oligos and its score. Recieves as input
         a pandas series with the oligos id as index and the score as value. The return is a list with the id of the selected
         oligos and the scores of the set.
-        REMARK: the set can have different scores with increasing relevance, where the less relevan scores are used in case of
-        ties. Therefore in case the fist scores are equal the sording is done according to the second one and so on.
+        REMARK: the set can have different scores with increasing relevance, where the less relevant scores are used in case of
+        ties. Therefore in case the fist scores are equal the sorting is done according to the second one and so on.
 
-        :param clique_oligos: Series of a set of non overlapping oligos
-        :type clique_oligos: pandas.Series
+        :param oligo_set: Series of a set of oligos
+        :type oligo_set: pandas.Series
         :param n: size of teh set
         :type n: int
         :return: optimal set and its score
@@ -24,23 +24,23 @@ class SetScoringBase(ABC):
 
 
 class PadlockSetScoring(SetScoringBase):
-    """Scoring class for the sets of oligos used for teh Padlock designer pipeline."""
+    """Scoring class for a set of oligos. Padlock probe sepcific scoring."""
 
     def __init__(self) -> None:
         pass
 
-    def apply(self, clique_oligos: pd.Series, n: int):
+    def apply(self, oligo_set: pd.Series, n: int):
         """From a set of non-overlapping oligos extracts the best subset of n oligos and its scores. The scores are, in order of relevance,
-         the maximal oligo score in the set and the avreage of the scores.
+         the maximal oligo score in the set and the sum of the scores.
 
-        :param clique_oligos: Series of a set of non overlapping oligos
-        :type clique_oligos: pandas.Series
-        :param n: size of teh set
+        :param oligo_set: Series of a set of non overlapping oligos
+        :type oligo_set: pandas.Series
+        :param n: size of the set
         :type n: int
         :return: optimal set and its score
         :rtype: list
         """
-        best_n_oligos = clique_oligos.sort_values(ascending=True).head(n)
+        best_n_oligos = oligo_set.sort_values(ascending=True).head(n)
         # Calculate performance of oligoset
         oligoset_error_max = best_n_oligos.max()
         oligoset_error_sum = best_n_oligos.sum()
@@ -50,23 +50,23 @@ class PadlockSetScoring(SetScoringBase):
 
 
 class AverageSetScoring(SetScoringBase):
-    """Scoring class for the sets of oligos. It retunrs as score the average of hte  oligo scores of the set."""
+    """Scoring class a set of oligos. Average oligo-score scoring of n oligos with highest scores."""
 
     def __init__(self) -> None:
         pass
 
-    def apply(self, clique_oligos: pd.Series, n: int):
-        """From a set of non-overlapping oligos extracts the best subset of n oligos and its scores.
-        The scores is the avreage of the scores of the oligoas in the set.
+    def apply(self, oligo_set: pd.Series, n: int):
+        """From a set of oligos extracts the best subset of n oligos and its scores.
+        The scores is the avreage of the scores of the oligos in the set.
 
-        :param clique_oligos: Series of a set of non overlapping oligos
-        :type clique_oligos: pandas.Series
-        :param n: size of teh set
+        :param oligo_set: Series of a set of oligos
+        :type oligo_set: pandas.Series
+        :param n: size of the set
         :type n: int
         :return: optimal set and its score
         :rtype: list
         """
-        best_n_oligos = clique_oligos.sort_values(ascending=True).head(n)
+        best_n_oligos = oligo_set.sort_values(ascending=True).head(n)
         oligoset_error_avg = best_n_oligos.mean()
         oligoset = best_n_oligos.index.tolist()
         oligoset += [oligoset_error_avg]
@@ -74,23 +74,23 @@ class AverageSetScoring(SetScoringBase):
 
 
 class MaxSetScoring(SetScoringBase):
-    """Scoring class for the sets of oligos. It retunrs as score the max  of the oligo scores of the set."""
+    """Scoring class a set of oligos. Maximum oligo-score scoring of n oligos with highest scores."""
 
     def __init__(self) -> None:
         pass
 
-    def apply(self, clique_oligos: pd.Series, n: int):
-        """From a set of non-overlapping oligos extracts the best subset of n oligos and its scores.
-        The scores is the max of the scores of the oligoa in the set.
+    def apply(self, oligo_set: pd.Series, n: int):
+        """From a set of oligos extracts the best subset of n oligos and its scores.
+        The scores is the max of the scores of the oligos in the set.
 
-        :param clique_oligos: Series of a set of non overlapping oligos
-        :type clique_oligos: pandas.Series
+        :param oligo_set: Series of a set of oligos
+        :type oligo_set: pandas.Series
         :param n: size of teh set
         :type n: int
         :return: optimal set and its score
         :rtype: list
         """
-        best_n_oligos = clique_oligos.sort_values(ascending=True).head(n)
+        best_n_oligos = oligo_set.sort_values(ascending=True).head(n)
         oligoset_error_avg = best_n_oligos.max()
         oligoset = best_n_oligos.index.tolist()
         oligoset += [oligoset_error_avg]
