b7bc803208151e9fe5f7321fb60a1eeb09540def
Francesco Campi <francesco.campi98@gmail.com>
2023-02-09 15:54:24 +0100

    rebase

--- docs/source/oligo_designer_toolsuite.database.rst
+++ docs/source/oligo_designer_toolsuite.database.rst
@@ -4,6 +4,14 @@ database
 Module contents
 ---------------
 
+.. .. currentmodule:: oligo_designer_toolsuite.database
+
+
+.. .. autosummary::
+..     CustomOligoDB
+..     NcbiOligoDB
+
+
 .. automodule:: oligo_designer_toolsuite.database
    :members:
    :undoc-members:
--- oligo_designer_toolsuite/database/__init__.py
+++ oligo_designer_toolsuite/database/__init__.py
@@ -1,5 +1,5 @@
 """
-This module contains the dataases of the oligo sequences and the reference file. In particular, it contains the classes to
+This module contains the databases of the oligo sequences and the reference file. In particular, it contains the classes to
 generate the oligos database and the reference database and allow to read ad write them to disk.
 """
 
