fab58b9f54d5ce6e4ebbcfa5578daba65ed93d82
hanane9 <58258128+hanane9@users.noreply.github.com>
2023-02-26 19:34:23 +0000

    debugging

--- oligo_designer_toolsuite/pipelines/__init__.py
+++ oligo_designer_toolsuite/pipelines/__init__.py
@@ -3,8 +3,8 @@ from ._padlock_probe_designer_config import padlock_probe_designer_config
 from ._merfish_target_designer import TargetProbes
 from ._merfish_readout_designer import ReadoutProbes
 from ._merfish_primer_designer import PrimerProbes
-from ._merfish_code_book import get_binary_sequences
+from ._merfish_code_book import generate_codebook
 from ._merfish_probe_designer import MerfishProbeDesigner
 
 #TargetProbes", "ReadoutProbes", "PrimerProbes",
-__all__ = ["padlock_probe_designer", "padlock_probe_designer_config","TargetProbes", "ReadoutProbes", "PrimerProbes","MerfishProbeDesigner","get_binary_sequences"]
+__all__ = ["padlock_probe_designer", "padlock_probe_designer_config","TargetProbes", "ReadoutProbes", "PrimerProbes","MerfishProbeDesigner","generate_codebook"]
