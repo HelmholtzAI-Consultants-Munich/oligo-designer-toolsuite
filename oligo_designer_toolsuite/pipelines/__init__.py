dc7710e0027ffe2e734630b2d451d75c80df2128
Lisa B. A. Sousa <lisa.barros.andrade.sousa@gmail.com>
2023-03-23 18:47:40 +0100

    add new classes to init

--- oligo_designer_toolsuite/pipelines/__init__.py
+++ oligo_designer_toolsuite/pipelines/__init__.py
@@ -1,7 +1,11 @@
 from ._padlock_probe_designer import PadlockProbeDesigner
+from ._padlock_probe_designer import MerfishProbeDesigner
+from ._padlock_probe_designer import SeqfishPlusProbeDesigner
 from ._padlock_probe_designer_config import padlock_probe_designer_config
 
 __all__ = [
     "PadlockProbeDesigner",
+    "MerfishProbeDesigner",
+    "SeqfishPlusProbeDesigner",
     "padlock_probe_designer_config",
 ]
