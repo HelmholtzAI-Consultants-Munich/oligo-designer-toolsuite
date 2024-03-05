d95a6dbca761dce00af3cd3b86670b4dac9bde66
Lisa Sousa <lisa.barros.andrade.sousa@gmail.com>
2024-03-05 18:55:09 +0100

    add functionalities and adapt imports

--- oligo_designer_toolsuite/utils/_utils.py
+++ oligo_designer_toolsuite/utils/_utils.py
@@ -12,8 +12,38 @@ from Bio.SeqUtils import MeltingTemp as mt
 ############################################
 
 
-def check_if_dna_sequence(seq: str, valid_characters={"A", "C", "T", "G"}) -> bool:
+def check_if_dna_sequence(seq: str, valid_characters=["A", "C", "T", "G"]):
+    """Verifies if a given sequence consists solely of valid DNA nucleotide characters (A, C, T, G).
+    This function is case-insensitive and validates the entire sequence against a set of allowed characters.
+
+    :param seq: The DNA sequence to validate.
+    :type seq: str
+    :param valid_characters: A list of characters representing valid nucleotides. Defaults to {"A", "C", "T", "G"}.
+    :type valid_characters: list
+    :return: True if the sequence is valid DNA, False otherwise.
+    :rtype: bool
+    """
     return all(char.upper() in valid_characters for char in seq)
+
+
+def check_if_key_exists(nested_dict: dict, key: str):
+    """Recursively check if a given key exists in a nested dictionary. The function searches through all levels of the nested dictionary to find the key.
+
+    :param nested_dict: The nested dictionary to search for the key.
+    :type nested_dict: dict
+    :param key: The key to search for within the nested dictionary.
+    :type key: str
+    :return: True if the key is found anywhere within the nested dictionary, False otherwise.
+    :rtype: bool
+    """
+    if isinstance(nested_dict, dict):
+        if key in nested_dict:
+            return True
+        else:
+            for value in nested_dict.values():
+                if check_if_key_exists(value, key):
+                    return True
+    return False
 
 
 def check_if_list(obj):
@@ -30,7 +60,7 @@ def check_if_list(obj):
     return obj
 
 
-def check_tsv_format(file):
+def check_tsv_format(file: str):
     """Check if a given TSV file has any content.
 
     :param file: The path to the TSV file.
@@ -43,6 +73,7 @@ def check_tsv_format(file):
         return any(read_tsv)
 
 
+# TODO: move to oligo database attributes class
 def get_TmNN(
     sequence: Seq,
     Tm_parameters: dict,
