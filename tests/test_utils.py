946ed386f499e6048794a11f4b8ee7d5fddebaac
IsraMekki0 <isra.mekki@helmholtz-muenchen.de>
2024-04-30 13:59:27 +0200

    Make tests utils work

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,6 +5,9 @@
 import unittest
 import warnings
 
+import pandas as pd
+from effidict import LRUDict
+
 from oligo_designer_toolsuite.utils import (
     FastaParser,
     GffParser,
@@ -29,7 +32,7 @@ class TestGffParser(unittest.TestCase):
         self.parser = GffParser()
         self.gff_file = "data/annotations/custom_GCF_000001405.40_GRCh38.p14_genomic_chr16.gff"
         self.gtf_file = "data/annotations/custom_GCF_000001405.40_GRCh38.p14_genomic_chr16.gtf"
-        self.pickle_file = ...
+        self.pickle_file = "data/tests/custom_GCF_000001405.40_GRCh38.p14_genomic_chr16_gtf.pickle"
 
     def test_check_gff_format(self):
         """Test parsing GFF annotation data."""
@@ -49,13 +52,6 @@ class TestGffParser(unittest.TestCase):
                 False
             ), f"error: checker: check_gff_format raised an exception: {e}, with file {self.gtf_file}"
 
-    # TODO
-    def test_load_annotation_from_pickle_file(self):
-        """Test loading annotation from a pickle file."""
-        expected_result = ...
-        result = self.parser.load_annotation_from_pickle(self.pickle_file)
-        assert result == expected_result, f"error: loading annotation from pickle file failed"
-
     def test_parse_annotation_from_gff(self):
         """Test parsing GFF annotation."""
         result = self.parser.parse_annotation_from_gff(self.gff_file, target_lines=10)
@@ -65,6 +61,11 @@ class TestGffParser(unittest.TestCase):
         """Test parsing GTF annotation."""
         result = self.parser.parse_annotation_from_gff(self.gtf_file, target_lines=10)
         assert result.shape[1] == 20, "error: GTF dataframe not correctly loaded"
+
+    def test_load_annotation_from_pickle_file(self):
+        """Test loading annotation from a pickle file."""
+        result = self.parser.load_annotation_from_pickle(self.pickle_file)
+        assert type(result) == pd.DataFrame, f"error: GTF dataframe not correctly loaded from pickle file"
 
 
 class TestFastaParser(unittest.TestCase):
@@ -81,33 +82,37 @@ class TestFastaParser(unittest.TestCase):
                 False
             ), f"error: checker: check_fasta_format raised an exception: {e}, with file {self.fasta_file}"
 
-    # TODO
     def test_read_fasta_sequences_existing_regions(self):
         """Test parsing fasta file."""
-        expected_result = ...
-        ids = ...
-
+        ids = ["16"]
         result = self.parser.read_fasta_sequences(self.fasta_file, region_ids=ids)
-        assert result == expected_result, "error: fasta dataframe not correctly loaded"
 
-    # TODO
+        assert len(result) == 1, f"error: the function loaded {len(result)} entries instead of 1"
+        assert result[0].name == "16", f"error: the name should be '16' instead of {result[0].name}"
+        assert (
+            result[0].description == "16 Homo sapiens chromosome 16, GRCh38.p14 Primary Assembly"
+        ), f"error: the description should be '16 Homo sapiens chromosome 16, GRCh38.p14 Primary Assembly' instead of {result[0].description}"
+        assert (
+            result[0].dbxrefs == []
+        ), f"error: the dbxrefs should be an empty list instead of {result[0].dbxrefs}"
+
     def test_read_fasta_sequences_non_existing_region(self):
         """Test parsing fasta file."""
-        expected_result = ...
-        ids = ...
-
+        ids = ["1"]
         result = self.parser.read_fasta_sequences(self.fasta_file, region_ids=ids)
         # check if a warning was raised
         with warnings.catch_warnings(record=True) as w:
             result = self.parser.read_fasta_sequences(self.fasta_file, region_ids=ids)
             assert len(w) > 0, "error: no warning was raised"
-        assert result == expected_result, "error: fasta dataframe not correctly loaded"
+        assert len(result) == 0, f"error: the function loaded {len(result)} entries instead of an empty list"
 
     def test_get_fasta_regions(self):
         """Test if the parser extracts fasta regions correctly."""
-        expected_result = ...
+        expected_result = ["16"]
         result = self.parser.get_fasta_regions(self.fasta_file)
-        assert result == expected_result, "error: fasta regions not correctly extracted"
+        assert (
+            result == expected_result
+        ), f"error: fasta regions not correctly extracted. Expected ['16'] got {result}"
 
     def test_parse_fasta_header(self):
         """Test if the parser extracts fasta header correctly."""
@@ -118,9 +123,10 @@ class TestFastaParser(unittest.TestCase):
         assert coordinates["start"] == [70265537], "error: wrong start parsed"
         assert coordinates["end"] == [70265662], "error: wrong end parsed"
         assert coordinates["strand"] == ["-"], "error: wrong strand parsed"
-        assert (
-            additional_information == "transcript_id=XM4581;exon_id=XM4581_exon1"
-        ), "error: wrong additional information parsed"
+        assert additional_information == {
+            "transcript_id": ["XM4581"],
+            "exon_id": ["XM4581_exon1"],
+        }, f"error: wrong additional information parsed: {additional_information}"
 
 
 class TestCheckers(unittest.TestCase):
@@ -132,35 +138,55 @@ class TestCheckers(unittest.TestCase):
         except Exception as e:
             assert False, f"error: checker: check_tsv_format raised an exception: {e}, with file {file_tsv}"
 
-    # TODO
-    def test_check_if_key_exists(self):
-        """Test if the parser extracts fasta header correctly."""
-        nested_dict = ...
-        key = ...
-        expected_result = ...
-        result = check_if_key_exists(nested_dict, key)
-        assert result == expected_result, "error: check_if_key_exists failed"
+    def test_check_if_key_exists_empty(self):
+        """Test the check_if_key_exists function with an empty cache."""
+        empty_dict = LRUDict()
+        assert not check_if_key_exists(empty_dict, "a"), "Failed: Should return False for empty dictionary"
+
+    def test_check_if_key_exists_flat(self):
+        """Test the check_if_key_exists function with a flat cache."""
+        flat_database = LRUDict()
+        flat_database.load_from_dict({"a": 1, "b": 2})
+
+        assert check_if_key_exists(flat_database, "a"), "Failed: Key 'a' should exist in flat_database"
+        assert not check_if_key_exists(
+            flat_database, "z"
+        ), "Failed: Key 'z' should not exist in flat_database"
+
+    def test_check_if_key_exists_nested(self):
+        """Test the check_if_key_exists function with a nested cache."""
+
+        nested_database = LRUDict()
+        nested_database.load_from_dict({"a": {"b": {"c": 1}}, "d": 2, "e": {"f": {"g": {"h": 3}}}})
+
+        assert check_if_key_exists(nested_database, "c"), "Failed: Key 'c' should exist in nested_database"
+        assert not check_if_key_exists(
+            nested_database, "z"
+        ), "Failed: Key 'z' should not exist in nested_database"
+        assert check_if_key_exists(
+            nested_database, "h"
+        ), "Failed: Key 'h' should exist deep within nested_database"
 
     def test_check_if_list_str(self):
-        """Test if the parser extracts fasta header correctly."""
+        """Test if check_if_list works correctly for a string."""
         s = "test"
         result = check_if_list(s)
         assert result == [s], f"error: check_if_list failed. Expected: [{s}], got: {result}"
 
     def test_check_if_list_list(self):
-        """Test if the parser extracts fasta header correctly."""
+        """Test if check_if_list works correctly for a list."""
         l = ["test", ["test2"]]
         result = check_if_list(l)
         assert result == l, f"error: check_if_list failed. Expected: {l}, got: {result}"
 
     def test_check_if_dna_sequence_valid(self):
-        """Test if the parser extracts fasta header correctly."""
+        """Test if check_if_dna_sequence works correctly for a valid DNA sequence."""
         seq = "GGctAAgTTCCaGTttGCA"
         valid_characters = ["A", "C", "T", "G"]
         assert check_if_dna_sequence(seq, valid_characters), "error: check_if_dna_sequence failed"
 
     def test_check_if_dna_sequence_invalid(self):
-        """Test if the parser extracts fasta header correctly."""
+        """Test if check_if_dna_sequence works correctly for an invalid DNA sequence."""
         seq = "GGctAAgTuuTCCaGTttGCA"
         valid_characters = ["A", "C", "T", "G", "W", "X"]
         assert not check_if_dna_sequence(
