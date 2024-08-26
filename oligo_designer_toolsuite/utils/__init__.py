6750f532eb509a435f2e3fb3c33e60896f642963
Lisa Sousa <lisa.barros.andrade.sousa@gmail.com>
2024-08-26 17:50:10 +0200

    update doc strings for utils

--- oligo_designer_toolsuite/utils/__init__.py
+++ oligo_designer_toolsuite/utils/__init__.py
@@ -1,31 +1,29 @@
 """
-This module provides utilities for processing genomic databases, parsing sequences,
-and performing various sequence-related tasks.
+This module provides utilities for processing databases, parsing sequences and checking different file or object formats.
 
 Classes:
 - FastaParser: Parses FASTA files and provides methods for extracting sequence information.
 - GffParser: Parses GFF/GTF files and provides methods for extracting annotation information.
+- CustomYamlDumper: Custom YAML dumper for serializing Python objects to YAML format.
 
 Functions for processing databases:
-- merge_databases: Merges two genomic databases.
-- collapse_info_for_duplicated_sequences: Collapses information for duplicated sequences
-  in a genomic database.
-- filter_dabase_for_region: Filters a genomic database for sequences overlapping a given region.
-- format_oligo_attributes: Format the entries of an oligo_attributes dictionary to be list of lists.
-- check_if_region_in_database: Checks if specified regions exist in the provided database.
-- flatten_attribute_list: Flatten a nested list of attributes into a single list.
+- check_if_region_in_database: Checks if specified regions exist in the provided OligoDatabase.
+- collapse_attributes_for_duplicated_sequences: Collapses information for duplicated sequences in a OligoDatabase
+- format_oligo_attributes: Formats the entries of an oligo_attributes dictionary into a list of lists.
+- merge_databases: Merges two OligoDatabase by combining their content based on sequence keys.
+- flatten_attribute_list: Flattens a nested list of attributes into a single list.
 
 Functions for processing sequences:
 - get_sequence_from_annotation: Retrieves sequences based on genomic coordinates from a BED file.
 - get_complement_regions: Computes complement regions for a given set of genomic regions.
 
 Functions for checking formats:
-- check_if_dna_sequence: Verifies a sequence consists only of valid DNA nucleotides (A, C, T, G), case-insensitively.
+- check_if_dna_sequence: Verifies that a sequence consists only of valid DNA nucleotides (A, C, T, G), case-insensitively.
 - check_if_key_exists: Recursively searches a nested dictionary to find if a specific key exists.
 - check_if_list: Ensures the input is a list, converting it to one if necessary.
-- check_if_list_of_lists: Check if the given item is a list of lists.
-- check_tsv_format: Checks a TSV file for content, verifying it's not empty.
-
+- check_if_list_of_lists: Ensures the input is a list of lists, converting it to one if necessary.
+- check_tsv_format: Checks a TSV file for content, verifying it is not empty.
+- generate_unique_filename: Generates a unique filename based on the current timestamp.
 """
 
 from ._checkers_and_helpers import (
--- oligo_designer_toolsuite/utils/_sequence_processor.py
+++ oligo_designer_toolsuite/utils/_sequence_processor.py
@@ -18,6 +18,24 @@ def get_sequence_from_annotation(
     nameOnly=False,
     name=False,
 ) -> None:
+    """
+    Extracts sequences from a reference FASTA file based on regions specified in a BED file using `bedtools getfasta`.
+
+    :param file_bed: Path to the BED file containing regions of interest.
+    :type file_bed: str
+    :param file_reference_fasta: Path to the reference FASTA file.
+    :type file_reference_fasta: str
+    :param file_fasta: Output FASTA file path to store extracted sequences.
+    :type file_fasta: str
+    :param split: Whether to split the sequences. Given BED12 input, extract and concatenate the sequences from the BED “blocks” (e.g., exons), defaults to False.
+    :type split: bool
+    :param strand: Whether to force strandedness. If the feature occupies the antisense strand, the sequence will be reverse complemented, defaults to False.
+    :type strand: bool
+    :param nameOnly: Whether to use the name field for the FASTA header, defaults to False.
+    :type nameOnly: bool
+    :param name: Whether to use the name field and coordinates for the FASTA header, defaults to False.
+    :type name: bool
+    """
     cmd = "bedtools getfasta"
     cmd += " -fi " + file_reference_fasta
     cmd += " -bed " + file_bed
@@ -35,6 +53,16 @@ def get_sequence_from_annotation(
 
 
 def get_complement_regions(file_bed_in: str, file_chromosome_length: str, file_bed_out: str) -> None:
+    """
+    Generates the complement of genomic regions specified in a BED file using `bedtools complement`.
+
+    :param file_bed_in: Path to the input BED file containing regions of interest.
+    :type file_bed_in: str
+    :param file_chromosome_length: Path to the file specifying chromosome lengths.
+    :type file_chromosome_length: str
+    :param file_bed_out: Path to the output BED file where complement regions will be saved.
+    :type file_bed_out: str
+    """
     cmd = "bedtools complement"
     cmd += " -i " + file_bed_in
     cmd += " -g " + file_chromosome_length
