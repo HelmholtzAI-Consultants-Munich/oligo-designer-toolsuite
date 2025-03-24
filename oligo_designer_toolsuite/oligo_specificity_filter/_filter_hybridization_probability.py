26c5e42992139b41b58fa74cd55700a431d1488e
lisa-sousa <lisa.barros.andrade.sousa@gmail.com>
2025-03-24 17:08:31 +0100

    rename file and restructure filter

--- oligo_designer_toolsuite/oligo_specificity_filter/_filter_hybridization_probability.py
+++ oligo_designer_toolsuite/oligo_specificity_filter/_filter_hybridization_probability.py
@@ -2,30 +2,26 @@
 # imports
 ############################################
 
-import os
-import pandas as pd
-
-from typing import List
 from joblib import Parallel, delayed
 from joblib_progress import joblib_progress
 from oligo_designer_toolsuite_ai_filters.api import APIHybridizationProbability
 
 from oligo_designer_toolsuite._constants import _TYPES_SEQ
-from oligo_designer_toolsuite.database import OligoDatabase, ReferenceDatabase
+from oligo_designer_toolsuite.database import OligoDatabase
 from oligo_designer_toolsuite.oligo_specificity_filter import (
-    AlignmentSpecificityFilter,
+    SpecificityFilterReference,
+    SpecificityFilterAlignment,
     BlastNFilter,
     BlastNSeedregionFilter,
     BlastNSeedregionLigationsiteFilter,
-    SpecificityFilterBase,
 )
 
 ############################################
-# Oligo AI Filter Classes
+# Oligo Hybridization Probability Filter Class
 ############################################
 
 
-class HybridizationProbabilityFilter(SpecificityFilterBase):
+class HybridizationProbabilityFilter(SpecificityFilterReference):
     """
     A filter for assessing hybridization probabilities of oligonucleotides using AI models.
 
@@ -46,6 +42,8 @@ class HybridizationProbabilityFilter(SpecificityFilterBase):
     :type threshold: float
     :param ai_filter_path: Path to the AI model used for hybridization probability predictions. If None, the default path will be used.
     :type ai_filter_path: str, optional
+    :param remove_hits: If True, oligos overlapping variants are removed. If False, they are flagged.
+    :type remove_hits: bool
     :param filter_name: Name of the filter for identification purposes.
     :type filter_name: str
     :param dir_output: Directory to store output files and temporary data.
@@ -54,14 +52,15 @@ class HybridizationProbabilityFilter(SpecificityFilterBase):
 
     def __init__(
         self,
-        alignment_method: AlignmentSpecificityFilter,
+        alignment_method: SpecificityFilterAlignment,
         threshold: float,
         ai_filter_path: str = None,
+        remove_hits: bool = True,
         filter_name: str = "hybridization_probability_filter",
         dir_output: str = "output",
     ) -> None:
         """Constructor for the HybridizationProbabilityFilter class."""
-        super().__init__(filter_name, dir_output)
+        super().__init__(remove_hits, filter_name, dir_output)
 
         self.alignment_method = alignment_method
         self.overwrite_output_format()
@@ -70,11 +69,22 @@ class HybridizationProbabilityFilter(SpecificityFilterBase):
         self.threshold = threshold
         self.model = APIHybridizationProbability(ai_filter_path=ai_filter_path)
 
+    def create_reference(self, n_jobs):
+        """
+        Creates a reference file and builds an index for alignment-based search.
+
+        :param n_jobs: Number of parallel jobs to use during the indexing process.
+        :type n_jobs: int
+        :return: The name of the created reference file.
+        :rtype: str
+        """
+        file_reference = self.alignment_method.create_reference(n_jobs=n_jobs)
+
+        return file_reference
+
     def apply(
         self,
         oligo_database: OligoDatabase,
-        reference_database: ReferenceDatabase,
-        sequence_type: _TYPES_SEQ,
         n_jobs: int = 1,
     ) -> OligoDatabase:
         """
@@ -86,10 +96,6 @@ class HybridizationProbabilityFilter(SpecificityFilterBase):
 
         :param oligo_database: The OligoDatabase containing the oligonucleotides and their associated attributes.
         :type oligo_database: OligoDatabase
-        :param reference_database: The ReferenceDatabase used for alignment.
-        :type reference_database: ReferenceDatabase
-        :param sequence_type: The type of sequence to be used for the filter calculations.
-        :type sequence_type: _TYPES_SEQ["oligo", "target"]
         :param n_jobs: The number of parallel jobs to use for processing.
         :type n_jobs: int
         :return: The filtered OligoDatabase with off-targets removed.
@@ -98,43 +104,35 @@ class HybridizationProbabilityFilter(SpecificityFilterBase):
         # When applying the filter we don't want to consider hits within the same region
         consider_hits_from_input_region = False
 
-        # Create reference and index file for search
-        # Defined in advance to avoid writing the file multiple times
-        file_reference = reference_database.write_database_to_fasta(
-            filename=f"db_reference_{self.filter_name}"
-        )
-        file_index = self.alignment_method._create_index(file_reference=file_reference, n_jobs=n_jobs)
+        # create reference database / index
+        file_reference = self.create_reference(n_jobs=n_jobs)
 
         # run search in parallel for each region
         region_ids = list(oligo_database.database.keys())
         name = " ".join(string.capitalize() for string in self.filter_name.split("_"))
         with joblib_progress(description=f"Specificity Filter: {name}", total=len(region_ids)):
             Parallel(n_jobs=n_jobs, prefer="threads", require="sharedmem")(
-                delayed(self._apply_region)(
-                    sequence_type=sequence_type,
+                delayed(self._run_filter)(
+                    region_id=region_id,
                     oligo_database=oligo_database,
                     file_reference=file_reference,
-                    file_index=file_index,
-                    region_id=region_id,
                     consider_hits_from_input_region=consider_hits_from_input_region,
+                    mode=int(self.remove_hits),
                 )
                 for region_id in region_ids
             )
 
-        os.remove(file_reference)
-        os.remove(file_reference + ".fai")
-        self.alignment_method._remove_index(file_index)
+        self.remove_reference(file_reference)
 
         return oligo_database
 
-    def _apply_region(
+    def _run_filter(
         self,
-        sequence_type: _TYPES_SEQ,
         oligo_database: OligoDatabase,
         file_reference: str,
-        file_index: str,
         region_id: str,
         consider_hits_from_input_region: bool,
+        mode: int,
     ) -> None:
         """
         Applies the hybridization probability filter to a specific region in the OligoDatabase.
@@ -143,80 +141,34 @@ class HybridizationProbabilityFilter(SpecificityFilterBase):
         likelihood to hybridize with off-target sequences in a reference database. The filtering process uses
         an alignment method and AI model to evaluate the sequences, removing those that meet the criteria for potential cross-hybridization.
 
-        :param sequence_type: The type of sequence to be used for filter calculations.
-        :type sequence_type: _TYPES_SEQ["oligo", "target"]
         :param oligo_database: The OligoDatabase containing the oligonucleotides and their associated attributes.
         :type oligo_database: OligoDatabase
-        :param file_reference: The path to the reference FASTA file used for the alignment.
+        :param file_reference: Path to the reference file used for alignment filtering.
         :type file_reference: str
-        :param file_index: The index file for the ReferenceDatabase.
-        :type file_index: str
         :param region_id: Region ID to process.
         :type region_id: str
         :param consider_hits_from_input_region: Whether to consider hits from the input region in the filtering process.
         :type consider_hits_from_input_region: bool
+        :param mode: Operation mode — 0: flag hits, 1: remove hits.
+        :type mode: int
         """
-        table_hits_region = self.alignment_method._run_filter(
-            sequence_type=sequence_type,
+        table_hits = self.alignment_method._run_filter(
             region_id=region_id,
-            oligo_database=oligo_database,
-            file_index=file_index,
-            consider_hits_from_input_region=consider_hits_from_input_region,
-        )
-
-        table_hits_region = self._filter_table_hits(
-            sequence_type=sequence_type,
-            table_hits=table_hits_region,
             oligo_database=oligo_database,
             file_reference=file_reference,
-            region_id=region_id,
+            consider_hits_from_input_region=consider_hits_from_input_region,
+            mode=2,
         )
-
-        oligos_with_hits_region = table_hits_region["query"].unique()
-        self._filter_hits_from_database(
-            oligo_database=oligo_database,
-            region_id=region_id,
-            oligos_with_hits=oligos_with_hits_region,
-        )
-
-    def _filter_table_hits(
-        self,
-        sequence_type: _TYPES_SEQ,
-        table_hits: pd.DataFrame,
-        oligo_database: OligoDatabase,
-        file_reference: str,
-        region_id: str,
-    ) -> pd.DataFrame:
-        """
-        Filters the hits from the alignment table based on hybridization probability.
-
-        This function processes the alignment results by filtering out hits with a hybridization probability
-        below a specified threshold. It generates aligned sequences (queries and references) with gaps,
-        predicts their hybridization probability, and retains only those hits that exceed the threshold.
-
-        :param sequence_type: The type of sequence to be used for filter calculations.
-        :type sequence_type: _TYPES_SEQ["oligo", "target"]
-        :param table_hits: A DataFrame containing the alignment results.
-        :type table_hits: pd.DataFrame
-        :param oligo_database: The OligoDatabase containing the oligonucleotides and their associated attributes.
-        :type oligo_database: OligoDatabase
-        :param file_reference: The path to the reference FASTA file used for the alignment.
-        :type file_reference: str
-        :param region_id: Region ID to process.
-        :type region_id: str
-        :return: A filtered DataFrame containing only the hits with hybridization probabilities above the threshold.
-        :rtype: pd.DataFrame
-        """
 
         # check if there are any oligos to filter
         if len(table_hits) == 0:
-            return table_hits
+            return
+
         # generate the references and queries sequences
         references = self.alignment_method._get_references(table_hits, file_reference, region_id)
         queries = self.alignment_method._get_queries(
             oligo_database=oligo_database,
             table_hits=table_hits,
-            sequence_type=sequence_type,
             region_id=region_id,
         )
         # align the references and queries by adding gaps
@@ -232,9 +184,31 @@ class HybridizationProbabilityFilter(SpecificityFilterBase):
             gapped_references=gapped_references,
         )
 
-        # filter the database, keep only the oligos above the threshold
+        # filter or flag hits in the database, keep only the oligos above the threshold
         table_hits = table_hits[predictions >= self.threshold]
-        return table_hits
+
+        if mode == 0:
+            oligos_with_hits_region = {region_id: table_hits["query"].unique()}
+            oligos_with_hits_region_attributes = (
+                table_hits.groupby("query")["reference"].apply(list).to_dict()
+            )
+            self._flag_hits_in_database(
+                oligo_database=oligo_database,
+                region_ids=region_id,
+                oligos_with_hits=oligos_with_hits_region,
+                oligos_with_hits_attributes=oligos_with_hits_region_attributes,
+            )
+        elif mode == 1:
+            oligos_with_hits_region = {region_id: table_hits["query"].unique()}
+            self._filter_hits_from_database(
+                oligo_database=oligo_database,
+                region_ids=region_id,
+                oligos_with_hits=oligos_with_hits_region,
+            )
+        else:
+            raise ValueError(
+                f"Mode {mode} not available. Choose mode=0 for removing hits from the database, mode=1 for flagging the hits in the database."
+            )
 
     def overwrite_output_format(self) -> None:
         """
@@ -243,9 +217,6 @@ class HybridizationProbabilityFilter(SpecificityFilterBase):
         This function modifies the output format of the alignment method to include specific fields required for subsequent processing.
         It checks if the alignment method is an instance of any of the BlastN-related classes, and if so, updates
         the `names_search_output` and `search_parameters["outfmt"]` to include detailed alignment information.
-
-        :param alignment_method: The alignment method object whose output format may need to be overwritten.
-        :type alignment_method: AlignmentSpecificityFilter
         """
         # if the alignment method is a  Blastn method overwrite the
         if type(self.alignment_method) in [
