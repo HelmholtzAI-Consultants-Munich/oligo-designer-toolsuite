692028687fdc60e6690ee1ba2001d33002b58413
Jonas Hagenberg <38041374+jonas-hag@users.noreply.github.com>
2026-07-13 17:17:06 +0200

    correct how error message is shown

--- oligo_designer_toolsuite/pipelines/_cycle_hcr_probe_designer.py
+++ oligo_designer_toolsuite/pipelines/_cycle_hcr_probe_designer.py
@@ -1823,7 +1823,7 @@ def main() -> None:
     try:
         config_validated = CycleHcrProbeDesignerConfig.model_validate(config_raw)
     except ValidationError as e:
-        print("Invalid configuration file:\n%s", e)
+        print(f"Invalid configuration file:\n{e}")
         raise
 
     # setup logger now that we know the output directory
